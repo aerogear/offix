5cb77adf79c0942c9b13609ae5abf7fa5573b867
Jose Miguel Gallas <josemigallas@gmail.com>
2018-04-13 17:16:01 +0200

    add some documentation

--- packages/core-cordova/www/metrics.js
+++ packages/core-cordova/www/metrics.js
@@ -2,20 +2,53 @@ const MetricsService = cordova.require("core.core").MetricsService;
 
 const MOBILE_CORE_CLASS = "MobileCoreModule";
 
+/**
+ * Represents a Metrics object
+ * @private
+ * @example
+ * var appMetrics = new MetricsImpl("app", { platform: "android" });
+ */
 class MetricsImpl {
 
+  /**
+   * Create a Metrics object.
+   * @param {string} id - A name identifying the kind of metrics.
+   * @param {object} data - An object containing all metrics and its values.
+   */
   constructor(id, data) {
     this.id = id;
     this.data = data;
   }
 
+  /**
+   * Get all metrics
+   * @public
+   * @returns {object} An object containing all metrics and its values.
+   */
   collect() {
     return this.data;
   }
 }
 
+/**
+ * Class that exposes the AeroGear metrics SDK for a Cordova application.
+ * It is globally available trough 'window.aerogear' or simply 'aerogear', under the name
+ * of MetricsService.
+ * @public
+ * @example
+ * var MetricsService = window.aerogear.MetricsService;
+ * @example
+ * var metricsService = new aerogear.MetricsService({ url: "http://my-service"});
+ * metricsService.sendAppAndDeviceMetrics()
+ *   .then(handleResponse)
+ */
 class CordovaMetricsService extends MetricsService {
 
+  /**
+   * Get some metrics about the application and device
+   * @public
+   * @return {Promise} A promise containing an object with the metrics
+   */
   getAppAndDeviceMetrics() {
     return new Promise((res, rej) => {
       cordova.exec(
@@ -27,11 +60,16 @@ class CordovaMetricsService extends MetricsService {
         },
         rej,
         MOBILE_CORE_CLASS,
-        "getMetrics"
+        "getAppAndDeviceMetrics"
       );
     });
   }
 
+  /**
+   * Send metrics about the application and device to the Metrics service.
+   * @public
+   * @returns {Promise} A promise containing the response from the server.
+   */
   sendAppAndDeviceMetrics() {
     return this.getAppAndDeviceMetrics()
       .then(metrics => {
@@ -42,11 +80,21 @@ class CordovaMetricsService extends MetricsService {
       });
   }
 
+  /**
+   * Get the device's client id if it stored, otherwise return undefined.
+   * @private
+   * @returns {string|undefined} The client id or undefined.
+   */
   getSavedClientId() {
     // TODO: get from device storage or return undefined
     return undefined;
   }
 
+  /**
+   * Save a string as this device's unique client id, so it can be identified by any AeroGear service.
+   * @private
+   * @param {string} id - The id to be saved
+   */
   saveClientId(id) {
     // TODO: store in device storage
     return;
