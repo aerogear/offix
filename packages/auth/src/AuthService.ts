d9350bb1ec6b5d775fcd876364f7a9749dae2c63
Wojciech Trocki <wtrocki@redhat.com>
2018-04-19 16:30:33 +0100

    Remove unused npm module

--- packages/auth/src/AuthService.ts
+++ packages/auth/src/AuthService.ts
@@ -1,63 +1,62 @@
 import Keycloak from "keycloak-js";
-import {KeycloakError, KeycloakInitOptions, KeycloakInstance, KeycloakProfile, KeycloakPromise} from "keycloak-js";
-import { Key } from "readline";
+import { KeycloakError, KeycloakInitOptions, KeycloakInstance, KeycloakProfile, KeycloakPromise } from "keycloak-js";
 
 /**
  * Wrapper class for {Keycloak.KeycloakInstance}
  */
 export class AuthService {
 
-    private auth: KeycloakInstance;
+  private auth: KeycloakInstance;
 
-    constructor(config: any) {
-      this.auth = Keycloak(config);
-    }
+  constructor(config: any) {
+    this.auth = Keycloak(config);
+  }
 
   /**
    * Called to initialize the adapter.
    * @param initOptions Initialization options.
    * @returns A promise to set functions to be invoked on success or error.
    */
-   public init(initOptions: KeycloakInitOptions): KeycloakPromise<boolean, KeycloakError> {
-      if (!initOptions.onLoad) {
-        initOptions.onLoad = "check-sso";
-      }
-      return this.auth.init(initOptions);
+  public init(initOptions: KeycloakInitOptions): KeycloakPromise<boolean, KeycloakError> {
+    if (!initOptions.onLoad) {
+      initOptions.onLoad = "check-sso";
     }
+    return this.auth.init(initOptions);
+  }
 
-    /**
-		 * Loads the user's profile.
-		 * @returns A promise to set functions to be invoked on success or error.
-		 */
-    public loadUserProfile(): KeycloakPromise<KeycloakProfile, void> {
-        return this.auth.loadUserProfile();
-    }
+  /**
+   * Loads the user's profile.
+   * @returns A promise to set functions to be invoked on success or error.
+   */
+  public loadUserProfile(): KeycloakPromise<KeycloakProfile, void> {
+    return this.auth.loadUserProfile();
+  }
 
-    /**
-		 * Redirects to login form.
-		 * @param options Login options.
-		 */
-    public login(): KeycloakPromise<void, void> {
-      return this.auth.login();
-    }
+  /**
+   * Redirects to login form.
+   * @param options Login options.
+   */
+  public login(): KeycloakPromise<void, void> {
+    return this.auth.login();
+  }
 
-    /**
-		 * Redirects to logout.
-		 * @param options Logout options.
-		 * @param options.redirectUri Specifies the uri to redirect to after logout.
-		 */
-    public logout(): KeycloakPromise<void, void> {
-      return this.auth.logout();
-    }
+  /**
+   * Redirects to logout.
+   * @param options Logout options.
+   * @param options.redirectUri Specifies the uri to redirect to after logout.
+   */
+  public logout(): KeycloakPromise<void, void> {
+    return this.auth.logout();
+  }
 
-    public isAuthenticated(): boolean | undefined {
-        return this.auth.authenticated;
-    }
+  public isAuthenticated(): boolean | undefined {
+    return this.auth.authenticated;
+  }
 
-    /**
-     * Get access to wrapped Keycloak object
-     */
-    public extract(): KeycloakInstance {
-      return this.auth;
-    }
+  /**
+   * Get access to wrapped Keycloak object
+   */
+  public extract(): KeycloakInstance {
+    return this.auth;
+  }
 }
