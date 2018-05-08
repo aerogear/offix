edf382aef6dbdd0fdb97ef64c284bc81c17f28d5
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-05-08 16:49:39 +0100

    fix lint errors

--- packages/security/src/deviceTrust/checks/DebuggerCheck.ts
+++ packages/security/src/deviceTrust/checks/DebuggerCheck.ts
@@ -3,20 +3,20 @@ import { SecurityCheckResult } from "../SecurityCheckResult";
 
 declare let cordova: any;
 
-export class DebuggerCheck implements SecurityCheck{
+export class DebuggerCheck implements SecurityCheck {
     public readonly name = "Is Debugger Check";
 
     public check(): Promise<SecurityCheckResult> {
         return new Promise((resolve, reject) => {
-            if(!cordova.plugins.isDebug){
+            if (!cordova.plugins.isDebug) {
                 reject(new Error("Could not find plugin isDebug."));
                 return;
             }
 
             cordova.plugins.isDebug((passed: boolean) => {
-                const result: SecurityCheckResult = {name: this.name,passed:  passed};
+                const result: SecurityCheckResult = {name: this.name, passed};
                 return resolve(result);
             }, (error: string) => reject(error));
-        })
+        });
     }
 }
