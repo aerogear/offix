b4cdb76072db6cccc2b4ef14f178bd9d9a6998b6
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-05-09 10:28:44 +0100

    requested changes

--- packages/security/src/deviceTrust/SecurityCheckType.ts
+++ packages/security/src/deviceTrust/SecurityCheckType.ts
@@ -8,5 +8,10 @@ export const notRooted = new NonRootedCheck();
  * Detect whether a device is running on an emulator
  */
 export const notEmulated = new NonEmulatedCheck();
+  /**
+   * A check for whether a debugger is attached to the current application
+   */
+export const IsDebuggerConnected = new DebuggerCheck();
 
-export const notDebugger = new DebuggerCheck();
+
+
--- packages/security/src/deviceTrust/checks/DebuggerCheck.ts
+++ packages/security/src/deviceTrust/checks/DebuggerCheck.ts
@@ -3,18 +3,24 @@ import { SecurityCheckResult } from "../SecurityCheckResult";
 
 declare let cordova: any;
 
+/**
+ * A check for whether a debugger is attached to the current application
+ */
 export class DebuggerCheck implements SecurityCheck {
     public readonly name = "Is Debugger Check";
 
+    /**
+     * An application running with an attached debugger can have its internals exposed.
+     */
     public check(): Promise<SecurityCheckResult> {
         return new Promise((resolve, reject) => {
-            if (!cordova.plugins.isDebug) {
+            if (!cordova.plugins.IsDebug) {
                 reject(new Error("Could not find plugin isDebug."));
                 return;
             }
 
-            cordova.plugins.isDebug((passed: boolean) => {
-                const result: SecurityCheckResult = {name: this.name, passed};
+            cordova.plugins.IsDebug.getIsDebug((passed: boolean) => {
+                const result: SecurityCheckResult = {name: this.name, passed: !passed};
                 return resolve(result);
             }, (error: string) => reject(error));
         });
