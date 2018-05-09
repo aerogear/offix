43fbde12df0ef845ffff345c7b3785341d1f10da
rachael-oregan <roregan@redhat.com>
2018-05-09 10:22:33 +0100

    AEROGEAR-2680 - add emulator security check

--- packages/security/src/deviceTrust/SecurityCheckType.ts
+++ packages/security/src/deviceTrust/SecurityCheckType.ts
@@ -1,6 +1,10 @@
-import { NonRootedCheck } from "./checks";
+import { NonRootedCheck, NonEmulatedCheck } from "./checks";
 
 /**
  * Detect whether a device is rooted (Android) or Jailbroken (iOS).
  */
 export const notRooted = new NonRootedCheck();
+/** 
+ * Detect whether a device is running on an emulator 
+ */
+export const notEmulated = new NonEmulatedCheck();
---
+++ packages/security/src/deviceTrust/checks/NonEmulatedCheck.ts
@@ -0,0 +1,19 @@
+import { SecurityCheck } from "../SecurityCheck";
+import { SecurityCheckResult } from "../SecurityCheckResult";
+
+declare var device: any;
+
+export class NonEmulatedCheck implements SecurityCheck {
+  public readonly name = "Emulator Check";
+
+  public check(): Promise<SecurityCheckResult> {
+    return new Promise((resolve, reject) => {
+      if (!device) {
+        reject(new Error("Could not find plugin device."));
+        return;
+      }
+      const result: SecurityCheckResult = { name: this.name, passed: !device.isVirtual };
+      return resolve(result);
+    })
+  }
+}
