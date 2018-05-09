9beacfbfd7a19d7c1de2b8b9799b3d7e6dd92fa5
rachael-oregan <roregan@redhat.com>
2018-05-09 10:51:50 +0100

    AEROGEAR-2677 - add device lock security check

--- packages/security/src/deviceTrust/SecurityCheckType.ts
+++ packages/security/src/deviceTrust/SecurityCheckType.ts
@@ -1,4 +1,4 @@
-import { NonDebugCheck, NonEmulatedCheck, NonRootedCheck } from "./checks";
+import { DeviceLockCheck, NonDebugCheck, NonEmulatedCheck, NonRootedCheck } from "./checks";
 
 /**
  * Detect whether a device is rooted (Android) or Jailbroken (iOS).
@@ -12,3 +12,7 @@ export const notEmulated = new NonEmulatedCheck();
    * Detect whether a device is running in debug mode.
    */
 export const notDebugMode = new NonDebugCheck();
+/**
+ * Detect whether a device has a screen lock set or not.
+ */
+export const hasDeviceLock = new DeviceLockCheck();
---
+++ packages/security/src/deviceTrust/checks/DeviceLockCheck.ts
@@ -0,0 +1,43 @@
+import { SecurityCheck } from "../SecurityCheck";
+import { SecurityCheckResult } from "../SecurityCheckResult";
+
+declare var cordova: any;
+
+/**
+ * Security check to detect if a device has a screen lock set or not.
+ */
+export class DeviceLockCheck implements SecurityCheck {
+  /**
+   * Get the name of the check.
+   */
+  get name(): string {
+    return "Device Lock Check";
+  }
+
+  /**
+   * Determine whether a device has a screen lock set or not.
+   * If the device *has* a screen lock set then the check
+   * will pass.
+   *
+   * @returns The result of the check.
+   */
+  public check(): Promise<SecurityCheckResult> {
+    return new Promise((resolve, reject) => {
+      if (!cordova || !cordova.plugins || !cordova.plugins.PinCheck) {
+        reject(new Error("Could not find plugin PinCheck"));
+      }
+
+      cordova.plugins.PinCheck.isPinSetup((pinIsSet: string) => {
+        const result: SecurityCheckResult = { name: this.name, passed: !!pinIsSet };
+        return resolve(result);
+      }, (error: Error) => {
+        if (error.toString() === "NO_PIN_SETUP") {
+          const result: SecurityCheckResult = { name: this.name, passed: !error };
+          return resolve(result);
+        } else {
+          return reject(error);
+        }
+      });
+    });
+  }
+}
