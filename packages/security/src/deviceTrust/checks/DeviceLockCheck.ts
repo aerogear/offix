94bcbbb5524a2e62263c8ef0e4b7df86912f6ae4
rachael-oregan <roregan@redhat.com>
2018-05-11 16:31:16 +0100

    add device ready checks

--- packages/security/src/deviceTrust/checks/DeviceLockCheck.ts
+++ packages/security/src/deviceTrust/checks/DeviceLockCheck.ts
@@ -2,6 +2,7 @@ import { SecurityCheck } from "../SecurityCheck";
 import { SecurityCheckResult } from "../SecurityCheckResult";
 
 declare var cordova: any;
+declare var document: any;
 
 /**
  * Security check to detect if a device has a screen lock set or not.
@@ -23,21 +24,27 @@ export class DeviceLockCheck implements SecurityCheck {
    */
   public check(): Promise<SecurityCheckResult> {
     return new Promise((resolve, reject) => {
-      if (!cordova || !cordova.plugins || !cordova.plugins.PinCheck) {
-        reject(new Error("Could not find plugin PinCheck"));
+      if (!document) {
+        reject(new Error("Cordova not fully loaded"));
       }
 
-      cordova.plugins.PinCheck.isPinSetup((pinIsSet: string) => {
-        const result: SecurityCheckResult = { name: this.name, passed: !!pinIsSet };
-        return resolve(result);
-      }, (error: Error) => {
-        if (error.toString() === "NO_PIN_SETUP") {
-          const result: SecurityCheckResult = { name: this.name, passed: !error };
-          return resolve(result);
-        } else {
-          return reject(error);
+      document.addEventListener("deviceready", () => {
+        if (!cordova || !cordova.plugins || !cordova.plugins.PinCheck) {
+          reject(new Error("Could not find plugin PinCheck"));
         }
-      });
+
+        cordova.plugins.PinCheck.isPinSetup((pinIsSet: string) => {
+          const result: SecurityCheckResult = { name: this.name, passed: !!pinIsSet };
+          return resolve(result);
+        }, (error: Error) => {
+          if (error.toString() === "NO_PIN_SETUP") {
+            const result: SecurityCheckResult = { name: this.name, passed: !error };
+            return resolve(result);
+          } else {
+            return reject(error);
+          }
+        });
+      }, false);
     });
   }
 }
