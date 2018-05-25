f8a117f7f5096a9678c43022fdb7093d3c222c97
Wojciech Trocki <wtrocki@redhat.com>
2018-05-25 12:30:46 +0100

    Cleanup implementations

---
+++ packages/push/src/PushConfig.ts
@@ -0,0 +1,12 @@
+
+export interface PushConfig {
+  "android": {
+    "senderId": string,
+    "variantId": string
+    "variantSecret": string
+  };
+  "ios": {
+    "variantId": string
+    "variantSecret": string
+  };
+}
