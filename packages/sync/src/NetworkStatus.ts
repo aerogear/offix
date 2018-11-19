838468bf26e9421ffac4498f8cba2b8dd1496549
Daniel Passos <daniel@passos.me>
2018-11-19 12:08:47 -0200

    Add NetworkStatus JavaScript doc

--- packages/sync/src/NetworkStatus.ts
+++ packages/sync/src/NetworkStatus.ts
@@ -1,9 +1,21 @@
 class NetworkStatus {
 
+  /**
+   *
+   * Trigger a function whenever the user switches into "Online Mode"
+   *
+   * @param fn Function to be called when got online
+   */
   public static whenOnline(fn: any): void {
     window.addEventListener("online", fn, false);
   }
 
+  /**
+   *
+   * Trigger a function whenever the user switches into "Offline Mode"
+   *
+   * @param fn Function to be called when got offline
+   */
   public static whenOffline(fn: any): void {
     window.addEventListener("offline", fn, false);
   }
