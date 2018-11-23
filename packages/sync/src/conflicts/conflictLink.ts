2aa50be1ba9b7c5ccbe68193b0375b1de04c69e9
Wojciech Trocki <wtrocki@redhat.com>
2018-11-23 11:46:30 +0000

    Check if conflict link data is present before processing conflict

--- packages/sync/src/conflicts/conflictLink.ts
+++ packages/sync/src/conflicts/conflictLink.ts
@@ -5,39 +5,41 @@ import { DataSyncConfig } from "../config/DataSyncConfig";
 import { ConflictResolutionData, ConflictResolutionStrategy, strategies } from "./strategies";
 
 export const conflictLink = (config: DataSyncConfig) => {
-    /**
-    * Fetch conflict data from the errors returned from the server
-    * @param graphQLErrors array of errors to retrieve conflicted data from
-    */
-    const getConflictData = (graphQLErrors?: ReadonlyArray<GraphQLError>): ConflictResolutionData => {
-        if (graphQLErrors) {
-            for (const err of graphQLErrors) {
-                if (err.extensions) {
-                    // TODO need to add flag to check if conflict was resolved on the server
-                    if (err.extensions.exception.type === "AgSync:DataConflict") {
-                        return err.extensions.exception.data;
-                    }
-                }
-            }
+  /**
+  * Fetch conflict data from the errors returned from the server
+  * @param graphQLErrors array of errors to retrieve conflicted data from
+  */
+  const getConflictData = (graphQLErrors?: ReadonlyArray<GraphQLError>): ConflictResolutionData => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions) {
+          // TODO need to add flag to check if conflict was resolved on the server
+          if (err.extensions.exception.type === "AgSync:DataConflict") {
+            return err.extensions.exception.data;
+          }
         }
-    };
-    /**
-     * Fetch the conflict strategy if one is provided, if not return client wins.
-     */
-    const getConflictStrategy = (): ConflictResolutionStrategy => {
-        if (config.conflictStrategy) {
-            return config.conflictStrategy;
-        } else {
-            return strategies.diffMergeClientWins;
-        }
-    };
+      }
+    }
+  };
+  /**
+   * Fetch the conflict strategy if one is provided, if not return client wins.
+   */
+  const getConflictStrategy = (): ConflictResolutionStrategy => {
+    if (config.conflictStrategy) {
+      return config.conflictStrategy;
+    } else {
+      return strategies.diffMergeClientWins;
+    }
+  };
 
-    return onError(({ operation, forward, graphQLErrors }) => {
-        const data = getConflictData(graphQLErrors);
-        const resolvedConflict = getConflictStrategy()(data, operation.variables);
-        // TODO Notify
-        resolvedConflict.version = data.version;
-        operation.variables = resolvedConflict;
-        return forward(operation);
-    });
+  return onError(({ operation, forward, graphQLErrors }) => {
+    const data = getConflictData(graphQLErrors);
+    if (data) {
+      const resolvedConflict = getConflictStrategy()(data, operation.variables);
+      // TODO Notify
+      resolvedConflict.version = data.version;
+      operation.variables = resolvedConflict;
+      return forward(operation);
+    }
+  });
 };
