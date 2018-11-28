0b1a903912e9f1d99fc8bee57936d4bfa510022d
Stephen Coady <scoady@redhat.com>
2018-11-28 12:58:56 +0000

    removed squash to separate function

--- packages/sync/src/links/QueueLink.ts
+++ packages/sync/src/links/QueueLink.ts
@@ -5,13 +5,13 @@ import {
   Observable,
   Operation
 } from "apollo-link";
-import { hasDirectives, getDirectiveNames } from "apollo-utilities";
+import { hasDirectives } from "apollo-utilities";
 import { Observer } from "zen-observable-ts";
 import { PersistedData, PersistentStore } from "../PersistentStore";
 import { Directives } from "../config/Constants";
-import { OperationDefinitionNode, NameNode } from "graphql";
 import { DataSyncConfig } from "../config/DataSyncConfig";
 import { NetworkStatus, NetworkInfo } from "../offline/NetworkStatus";
+import { squashOperations } from "../offline/squashOperations";
 
 export interface OperationQueueEntry {
   operation: Operation;
@@ -65,46 +65,8 @@ export default class QueueLink extends ApolloLink {
   }
 
   private enqueue(entry: OperationQueueEntry) {
-    this.squashOperations(entry);
+    this.opQueue = squashOperations(entry, this.opQueue);
     this.storage.setItem(this.key, JSON.stringify(this.opQueue));
-  }
-
-  /**
-  * Merge offline operations that are made on the same object.
-  * Equality of operation is done by checking operationName and object id.
-  */
-  private squashOperations(entry: OperationQueueEntry): OperationQueueEntry[] {
-    const { query, variables } = entry.operation;
-    let operationName: NameNode;
-
-    if (query.definitions[0]) {
-      const operationDefinition = query.definitions[0] as OperationDefinitionNode;
-      if (operationDefinition.name) {
-        operationName = operationDefinition.name;
-      }
-    }
-    const objectID = variables.id;
-    if (this.opQueue.length > 0 && objectID) {
-      // find the index of the operation in the array matching the incoming one
-      const index = this.opQueue.findIndex(queueEntry => {
-        if (queueEntry.operation.operationName === operationName.value &&
-            queueEntry.operation.variables.id === objectID) {
-          return true;
-        }
-        return false;
-      });
-      // if not found, add new operation directly
-      if (index === -1) {
-        this.opQueue.push(entry);
-      } else {
-        // else if found, merge the variables
-        const newOperationVariables = Object.assign(this.opQueue[index].operation.variables, variables);
-        this.opQueue[index].operation.variables = newOperationVariables;
-      }
-    } else {
-      this.opQueue.push(entry);
-    }
-    return this.opQueue;
   }
 
   private setNetworkStateHandlers(): void {
---
+++ packages/sync/src/offline/squashOperations.ts
@@ -0,0 +1,40 @@
+import { OperationDefinitionNode, NameNode } from "graphql";
+
+import { OperationQueueEntry } from "../links/QueueLink";
+/**
+ * Merge offline operations that are made on the same object.
+ * Equality of operation is done by checking operationName and object id.
+ */
+export function squashOperations(entry: OperationQueueEntry, opQueue: OperationQueueEntry[]): OperationQueueEntry[] {
+  const { query, variables } = entry.operation;
+  let operationName: NameNode;
+
+  if (query.definitions[0]) {
+    const operationDefinition = query.definitions[0] as OperationDefinitionNode;
+    if (operationDefinition.name) {
+      operationName = operationDefinition.name;
+    }
+  }
+  const objectID = variables.id;
+  if (opQueue.length > 0 && objectID) {
+    // find the index of the operation in the array matching the incoming one
+    const index = opQueue.findIndex(queueEntry => {
+      if (queueEntry.operation.operationName === operationName.value &&
+        queueEntry.operation.variables.id === objectID) {
+        return true;
+      }
+      return false;
+    });
+    // if not found, add new operation directly
+    if (index === -1) {
+      opQueue.push(entry);
+    } else {
+      // else if found, merge the variables
+      const newOperationVariables = Object.assign(opQueue[index].operation.variables, variables);
+      opQueue[index].operation.variables = newOperationVariables;
+    }
+  } else {
+    opQueue.push(entry);
+  }
+  return opQueue;
+}
