2d1e557f735204761a4924967c185d6b1cefae33
Wojciech Trocki <wtrocki@redhat.com>
2018-12-13 12:32:42 +0000

    Title update

--- packages/sync/test/createOptimisticResponseTest.ts
+++ packages/sync/test/createOptimisticResponseTest.ts
@@ -1,10 +1,10 @@
-import { createNewOptimisticResponse } from "../src/helpers/createOptimisticResponse";
+import { createOptimisticResponse } from "../src/helpers/createOptimisticResponse";
 import { expect } from "chai";
 
 describe("SquashOperations", () => {
 
   it("check createNewOptimisticResponse", () => {
-    const result = createNewOptimisticResponse("updateTest", "Test", { name: "test" });
+    const result = createOptimisticResponse("updateTest", "Test", { name: "test" });
     expect(result.updateTest.__typename).eq("Test");
     expect(result.updateTest.name).eq("test");
   });
