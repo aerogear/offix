da8f4fb83b1dd7e3338f4443241856f31dd7e204
Wojciech Trocki <wtrocki@redhat.com>
2019-01-04 17:07:41 +0000

    feat: conflict handling implementation

--- packages/sync/src/conflicts/conflictLink.ts
+++ packages/sync/src/conflicts/conflictLink.ts
@@ -1,9 +1,8 @@
 import { onError } from "apollo-link-error";
 import { GraphQLError } from "graphql";
 import { DataSyncConfig } from "../config";
-import { CONFLICT_ERROR } from "../config/Constants";
-import { ConflictResolutionData, ConflictResolutionStrategy, strategies } from "./strategies";
 import { ApolloLink } from "apollo-link";
+import { ConflictResolutionData } from "./ConflictResolutionData";
 
 export const conflictLink = (config: DataSyncConfig): ApolloLink => {
   /**
@@ -15,30 +14,22 @@ export const conflictLink = (config: DataSyncConfig): ApolloLink => {
       for (const err of graphQLErrors) {
         if (err.extensions) {
           // TODO need to add flag to check if conflict was resolved on the server
-          if (err.extensions.exception.type === CONFLICT_ERROR) {
-            return err.extensions.exception.data;
+          if (err.extensions.exception.conflictInfo) {
+            return err.extensions.exception.conflictInfo;
           }
         }
       }
     }
   };
-  /**
-   * Fetch the conflict strategy if one is provided, if not return client wins.
-   */
-  const getConflictStrategy = (): ConflictResolutionStrategy => {
-    if (config.conflictStrategy) {
-      return config.conflictStrategy;
-    } else {
-      return strategies.diffMergeClientWins;
-    }
-  };
 
   return onError(({ operation, forward, graphQLErrors }) => {
     const data = getConflictData(graphQLErrors);
-    if (data) {
-      const resolvedConflict = getConflictStrategy()(data, operation.variables);
-      // TODO Notify
-      resolvedConflict.version = data.version;
+    if (data && config.conflictStrategy && config.conflictStateProvider) {
+      let resolvedConflict = config.conflictStrategy(operation.operationName, data, operation.variables);
+      resolvedConflict = config.conflictStateProvider.nextState(resolvedConflict);
+      if (config.conflictListener) {
+        config.conflictListener.conflictOccurred(operation.operationName, resolvedConflict, data, operation.variables);
+      }
       operation.variables = resolvedConflict;
       return forward(operation);
     }
