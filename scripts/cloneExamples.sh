aea0f056bbf3f622f3d7ca938f292011990f8d26
Wojtek Trocki <wtrocki@redhat.com>
2019-03-27 20:55:33 +0000

    fix: remove outdated script

--- scripts/cloneExamples.sh
+++
@@ -1,9 +0,0 @@
-set -eo pipefail
-
-echo "Development script that clones example application to be available in the repository"
-git clone git@github.com:aerogear/cordova-showcase-template.git cordova-example
-
-cd cordova-example
-npm install
-
-echo "Example application cloned with success."
