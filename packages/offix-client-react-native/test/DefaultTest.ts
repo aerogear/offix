6e279e10be9c5a278e183dabf854df12de189572
Wojciech Trocki <wtrocki@redhat.com>
2019-04-22 21:21:27 +0200

    fix: create temporary test

---
+++ packages/offix-client-react-native/test/DefaultTest.ts
@@ -0,0 +1,9 @@
+import { createOptimisticResponse } from "../src";
+import { expect, should } from "chai";
+
+describe("React Native tests", () => {
+
+  it("Empty test", () => {
+    console.log("Ignore")
+  });
+});
