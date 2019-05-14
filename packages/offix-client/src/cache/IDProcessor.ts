7cc1707540698a38eeae12785dba809da9ae1745
Wojciech Trocki <wtrocki@redhat.com>
2019-05-15 00:42:09 +0100

    fix: Offline processors

---
+++ packages/offix-client/src/cache/IDProcessor.ts
@@ -0,0 +1,32 @@
+import { IResultProcessor } from "../offline/procesors/IResultProcessor";
+import { OperationQueueEntry } from "../offline/OperationQueueEntry";
+import { FetchResult } from "apollo-link";
+import { isClientGeneratedId } from "./createOptimisticResponse";
+
+  /**
+   * Allow updates on items created while offline.
+   * If item is created while offline and client generated ID is provided
+   * to optimisticResponse, later mutations on this item will be using this client
+   * generated ID. Once any create operation is successful, we should
+   * update entries in queue with ID returned from server.
+   */
+  export class IDProcessor implements IResultProcessor {
+
+    public execute(queue: OperationQueueEntry[], entry: OperationQueueEntry, result: FetchResult) {
+        const { operation: { operationName }, optimisticResponse } = entry;
+        if (!result ||
+          !optimisticResponse ||
+          !optimisticResponse[operationName] ||
+          !isClientGeneratedId(optimisticResponse[operationName].id)) {
+          return;
+        }
+    
+        const clientId = optimisticResponse && optimisticResponse[operationName].id;
+    
+        queue.forEach(({ operation: op }) => {
+          if (op.variables.id === clientId) {
+            op.variables.id = result.data && result.data[operationName].id;
+          }
+        });
+      }
+    }
---
+++ packages/offix-client/src/conflicts/ConflictProcesor.ts
@@ -0,0 +1,34 @@
+import { IResultProcessor } from "../offline/procesors/IResultProcessor";
+import { OperationQueueEntry } from "../offline/OperationQueueEntry";
+import { ObjectState } from "./ObjectState";
+import { FetchResult } from "apollo-link";
+
+/**
+ * Manipulate state of item that is being used for conflict resolution purposes.
+ * This is required for the queue items so that we do not get a conflict with ourself
+ * @param entry the operation which returns the result we compare with first queue entry
+ */
+export class ConflictProcessor implements IResultProcessor {
+    constructor(private state: ObjectState) {
+    }
+
+    execute(queue: OperationQueueEntry[], entry: OperationQueueEntry, result: FetchResult): void {
+        const { operation: { operationName } } = entry;
+        if (!result || !this.state) {
+            return;
+        }
+
+        if (result.data && result.data[operationName]) {
+            for (const { operation: op } of queue) {
+                if (op.variables.id === entry.operation.variables.id && op.operationName === entry.operation.operationName) {
+                    const opVersion = this.state.currentState(op.variables);
+                    const prevOpVersion = this.state.currentState(entry.operation.variables);
+                    if (opVersion === prevOpVersion) {
+                        op.variables = this.state.nextState(op.variables);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
