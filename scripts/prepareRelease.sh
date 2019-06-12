4e48e61ec0339665f535f45910c19a6ff91babc5
Wojciech Trocki <wtrocki@redhat.com>
2019-06-12 13:29:37 +0100

    chore: release 0.6.0

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -13,7 +13,6 @@ npm run test
 # don't run in CI
 if [ ! "$CI" = true ]; then
   lerna publish --skip-git --force-publish=* --skip-npm
-  npm run updateXml
 fi
 
 echo "Repository is ready for release."
