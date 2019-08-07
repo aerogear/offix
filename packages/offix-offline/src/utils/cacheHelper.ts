2936ba59c27fad55251001ab65482e569cf277d2
Stephen Coady <scoady@redhat.com>
2019-08-07 16:03:54 +0100

    fix: stop using grandparent

--- packages/offix-offline/src/utils/cacheHelper.ts
+++ packages/offix-offline/src/utils/cacheHelper.ts
@@ -10,17 +10,15 @@ const logger = debug.default("Offix:Helpers");
 export const getObjectFromCache = (operation: Operation, id: string) => {
     const context = operation.getContext();
 
-    if (context.cache && context.cache.data) {
+    if (context.cache && context.cache.data && !context.isOffline) {
       const idKey = context.getCacheKey({ __typename: context.returnType, id });
       if (context.cache.optimisticData && context.cache.optimisticData.parent) {
         let optimisticData;
-        if (context.cache.optimisticData.parent.parent) {
-          optimisticData = context.cache.optimisticData.parent.parent.data;
-        } else {
+        if (context.cache.optimisticData.parent) {
           optimisticData = context.cache.optimisticData.parent.data;
-        }
-        if (idKey && optimisticData[idKey]) {
-          return Object.assign({}, optimisticData[idKey]);
+          if (idKey && optimisticData[idKey]) {
+            return Object.assign({}, optimisticData[idKey]);
+          }
         }
       }
       const cacheData = context.cache.data.data;
