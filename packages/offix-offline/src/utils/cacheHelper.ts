2847fd9353ab2b8ff2bd4e408a9f2640fc4c7012
Stephen Coady <scoady@redhat.com>
2019-08-08 12:17:49 +0100

    fix: refactor tests and offline logic

--- packages/offix-offline/src/utils/cacheHelper.ts
+++ packages/offix-offline/src/utils/cacheHelper.ts
@@ -1,4 +1,3 @@
-
 import debug from "debug";
 import { Operation } from "apollo-link";
 
@@ -8,25 +7,24 @@ const logger = debug.default("Offix:Helpers");
  * Reads object from cache
  */
 export const getObjectFromCache = (operation: Operation, id: string) => {
-    const context = operation.getContext();
+  const context = operation.getContext();
 
-    if (context.cache && context.cache.data && !context.isOffline) {
+  if (context.cache && context.cache.data) {
+    if (!context.isOffline) {
       const idKey = context.getCacheKey({ __typename: context.returnType, id });
       if (context.cache.optimisticData && context.cache.optimisticData.parent) {
-        let optimisticData;
-        if (context.cache.optimisticData.parent) {
-          optimisticData = context.cache.optimisticData.parent.data;
-          if (idKey && optimisticData[idKey]) {
-            return Object.assign({}, optimisticData[idKey]);
-          }
+        const optimisticData = context.cache.optimisticData.parent.data;
+        if (idKey && optimisticData[idKey]) {
+          return Object.assign({}, optimisticData[idKey]);
         }
       }
       const cacheData = context.cache.data.data;
       if (idKey && cacheData[idKey]) {
         return Object.assign({}, cacheData[idKey]);
       }
-    } else {
-      logger("Client is missing cache implementation. Conflict features will not work properly");
     }
-    return {};
-  };
+  } else {
+    logger("Client is missing cache implementation. Conflict features will not work properly");
+  }
+  return {};
+};
