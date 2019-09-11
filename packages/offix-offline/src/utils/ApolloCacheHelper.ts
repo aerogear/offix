5a3888e2c31b8a8029fc66c440234ecd1989b492
Dara Hayes <dara.hayes@redhat.com>
2019-09-11 12:41:11 +0100

    fix: remove commented code

--- packages/offix-offline/src/utils/ApolloCacheHelper.ts
+++ packages/offix-offline/src/utils/ApolloCacheHelper.ts
@@ -40,31 +40,3 @@ export class ApolloCacheHelper {
     return {};
   }
 }
-
-// /**
-//  * Reads object from cache
-//  */
-// export const getObjectFromCache = (operation: Operation, id: string) => {
-
-//   if (context.cache && context.cache.data) {
-//     if (!context.isOffline) {
-//       const idKey = context.cache.config.dataIdFromObject({ __typename: context.returnType, id })
-//       // const idKey = context.getCacheKey({ __typename: context.returnType, id });
-//       if (context.cache.optimisticData && context.cache.optimisticData.parent) {
-//         const optimisticData = context.cache.optimisticData.parent.data;
-//         if (idKey && optimisticData[idKey]) {
-//           // return copy of original object
-//           return Object.assign({}, optimisticData[idKey]);
-//         }
-//       }
-//       const cacheData = context.cache.data.data;
-//       if (idKey && cacheData[idKey]) {
-//         // return copy of original object
-//         return Object.assign({}, cacheData[idKey]);
-//       }
-//     }
-//   } else {
-//     logger("Client is missing cache implementation. Conflict features will not work properly");
-//   }
-//   return {};
-// };
