160939f272f37ba91b6f783e7865f9b8b74231ae
Dara Hayes <dara.hayes@redhat.com>
2019-09-19 11:09:26 +0100

    fix: remove optimistic response on error

--- packages/offix-client/src/cache.ts
+++ packages/offix-client/src/cache.ts
@@ -1,7 +1,4 @@
 import { Store } from "idb-localstorage";
-import { persistCache } from "apollo-cache-persist";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { PersistedData, PersistentStore } from "offix-offline";
 
 export const createDefaultCacheStorage = () => {
   return new Store("apollo-cache", "cache-store");
