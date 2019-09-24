00540b5d5ec3eee0b0f5527bce8ad2759cb79fa7
Dara Hayes <dara.hayes@redhat.com>
2019-09-24 17:37:22 +0100

    fix: integrate jest inside offix-client

---
+++ packages/offix-client/test/OffixClientConfig.test.ts
@@ -0,0 +1,33 @@
+import "fake-indexeddb/auto";
+import { OffixDefaultConfig } from "../src/config/OffixDefaultConfig";
+import { OffixClientConfig } from "../src/config/OffixClientConfig";
+
+test("OffixDefaultConfig Merges defaults with user config", () => {
+  
+  const userConfig = {
+    httpUrl: "test",
+    // storage,
+    retryOptions: {
+      attempts: {
+        max: 10
+      }
+    }
+  };
+  
+  const config = new OffixDefaultConfig(userConfig);
+  expect(config.httpUrl).toBe(userConfig.httpUrl);
+  expect(config.retryOptions).toBe(userConfig.retryOptions);
+});
+
+it("conflict strategy is a function", () => {
+  const configWithStrategy: OffixClientConfig = {
+    httpUrl: "test",
+    // storage,
+    conflictStrategy: {
+      resolve: ({ base, server, client }) => server
+    }
+  };
+
+  const mergedConfig = new OffixDefaultConfig(configWithStrategy);
+  expect(mergedConfig.conflictStrategy).toBe(configWithStrategy.conflictStrategy)
+});
---
+++ packages/offix-client/test/OfflineClient.api.test.ts
@@ -0,0 +1,63 @@
+// polyfills that let us test in node
+import "fake-indexeddb/auto";
+import 'cross-fetch/polyfill';
+
+import { HttpLink } from "apollo-link-http";
+import { OfflineClient, createClient, OfflineQueueListener } from '../src'
+import ApolloClient, { MutationOptions } from 'apollo-client';
+
+test('createClient does not throw', async () => {
+  const url = 'http://test'
+  
+  const client = await createClient({
+    httpUrl: url,
+    // storage,
+    // networkStatus
+  });
+
+  expect(client).toBeDefined();
+  expect(client instanceof ApolloClient).toBe(true)
+});
+
+test("OfflineClient constructor does not throw", async () => {
+  const url = 'http://test'
+  const client = new OfflineClient({ 
+    httpUrl: url 
+  });
+    const initClient = await client.init();
+    expect(client.apolloClient).toBeDefined();
+    expect(initClient.offlineStore).toBeDefined();
+    expect(client.registerOfflineEventListener).toBeDefined();
+})
+
+test("OfflineClient using terminatingLink", async () => {
+  const url = 'http://test'
+  const terminatingLink = new HttpLink({ uri: url });
+  const client = new OfflineClient({ terminatingLink });
+  await client.init();
+  expect(client.apolloClient).toBeDefined();
+  expect(client.store).toBeDefined();
+})
+
+test("client.init() throws when invalid config is given", async () => {
+  //@ts-ignore
+  const client = new OfflineClient()
+  await expect(client.init()).rejects.toThrow('Missing url');
+})
+
+test("registerOfflineEventListener adds the listener to the queue listeners", async () => {
+  const url = 'http://test'
+  const client = new OfflineClient({ 
+    httpUrl: url 
+  });
+  await client.init();
+  
+  expect(client.queue.listeners.length).toBe(1) // the default one added in by the client
+  
+  const listener: OfflineQueueListener<MutationOptions> = {
+    onOperationEnqueued: (op) => {}
+  }
+  client.registerOfflineEventListener(listener)
+  expect(client.queue.listeners.length).toBe(2)
+  expect(client.queue.listeners[1]).toBe(listener)
+})
