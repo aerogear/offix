0bfc156994fe58e55881dd57c6c415cc700392a0
Dara Hayes <dara.hayes@redhat.com>
2019-10-04 11:01:03 +0100

    chore: document the apollo queue and store types

--- packages/offix-client/src/apollo/ApolloOfflineClient.ts
+++ packages/offix-client/src/apollo/ApolloOfflineClient.ts
@@ -11,11 +11,45 @@ import {
 } from "offix-offline";
 import { MutationHelperOptions } from "offix-cache";
 
+/**
+ * An OfflineQueue instance for queueing Apollo Client mutations.
+ * The QueueEntries hold MutationOptions objects
+ * which are passed into ApolloClient.mutate()
+ */
 export type ApolloOfflineQueue = OfflineQueue<MutationOptions>;
+
+/**
+ * An OfflineStore instance for persisting Apollo Client Mutations.
+ * The store persists MutationOptions objects
+ * which can be restored to the OfflineQueue.
+ */
 export type ApolloOfflineStore = OfflineStore<MutationOptions>;
+
+/**
+ * An interface for reacting to events fired by the
+ * ApolloOfflineQueue.
+ */
 export type ApolloOfflineQueueListener = OfflineQueueListener<MutationOptions>;
+
+/**
+ * A IResultProcessor that specifically deals with the result
+ * from ApolloClient.mutate()
+ */
 export type ApolloIResultProcessor = IResultProcessor<MutationOptions>;
+
+/**
+ * The top level entry held in the ApolloOfflineQueue.
+ * It holds a MutationOptions object which is passed into
+ * ApolloClient.mutate() and also holds the resolve/reject
+ * Promise handlers for OfflineClient.offlineMutate()
+ */
 export type ApolloQueueEntry = QueueEntry<MutationOptions>;
+
+/**
+ * Found inside an ApolloQueueEntry object.
+ * The ApolloQueueEntryOperation directly holds the
+ * MutationOptions object passed into ApolloClient.mutate()
+ */
 export type ApolloQueueEntryOperation = QueueEntryOperation<MutationOptions>;
 
 /**
