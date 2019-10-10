c96e405362ec713eadf13ae2030c9fd9898b5021
pyitphyoaung <pyitphyoaung@users.noreply.github.com>
2019-10-11 07:02:54 +1100

    migrate tests for offix-conflicts-server from ava to jest (#194)

--- packages/offix-conflicts-server/test/VersionedObjectState.test.ts
+++ packages/offix-conflicts-server/test/VersionedObjectState.test.ts
@@ -1,28 +1,25 @@
-import test from "ava";
 import { VersionedObjectState } from "../src/states/VersionedObjectState";
 
-test("With conflict", (t) => {
+test("With conflict", () => {
   const objectState = new VersionedObjectState();
   const serverData = { name: "AeroGear", version: 1 };
   const clientData = { name: "Red Hat", version: 2 };
-  t.true(objectState.checkForConflict(serverData, clientData) !==  undefined);
+  expect(objectState.checkForConflict(serverData, clientData)).not.toBe(undefined);
 });
 
-test("Without conflict", (t) => {
+test("Without conflict", () => {
   const objectState = new VersionedObjectState();
   const serverData = { name: "AeroGear", version: 1 };
   const clientData = { name: "AeroGear", version: 1 };
 
-  t.true(objectState.checkForConflict(serverData, clientData) === undefined);
-  t.deepEqual(clientData.version, 2);
+  expect(objectState.checkForConflict(serverData, clientData)).toBe(undefined);
+  expect(clientData.version).toBe(2);
 });
 
-test("Missing version", (t) => {
+test("Missing version", () => {
   const objectState = new VersionedObjectState();
   const serverData = { name: "AeroGear" };
   const clientData = { name: "AeroGear", version: 1 };
 
-  t.throws(() => {
-    objectState.checkForConflict(serverData, clientData);
-  });
+  expect(() => objectState.checkForConflict(serverData, clientData)).toThrow();
 });
