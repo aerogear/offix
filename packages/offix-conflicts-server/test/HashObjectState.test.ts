f155f632cfba0dc39e5b9695c08a6e6d8083e401
Pyit Phyo Aung <aung.developer@gmail.com>
2019-10-11 05:58:59 +1100

    migrate tests for offix-conflicts-server from ava to jest

--- packages/offix-conflicts-server/test/HashObjectState.test.ts
+++ packages/offix-conflicts-server/test/HashObjectState.test.ts
@@ -1,11 +1,9 @@
-import test from "ava";
 import { HashObjectState } from "../src/states/HashObjectState";
 
-test("With conflict", (t) => {
+test("With conflict", () => {
   const hashMethod = (data: any) => JSON.stringify(data);
   const objectState = new HashObjectState(hashMethod);
   const serverData = { name: "AeroGear", ignoredProperty: "test", version: 1 };
   const clientData = { name: "Red Hat", version: 1};
-  t.true(objectState.checkForConflict(serverData, clientData) !==  undefined);
-
+  expect(objectState.checkForConflict(serverData, clientData)).not.toBe(undefined);
 });
