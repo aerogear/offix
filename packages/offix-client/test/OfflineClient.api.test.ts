ec9d1f6032f94d0d3f422e552dd68ef22ff88c55
Dara Hayes <dara.s.hayes@gmail.com>
2019-10-15 11:24:32 +0100

    feat: allow users to provide their own InMemoryCache

--- packages/offix-client/test/OfflineClient.api.test.ts
+++ packages/offix-client/test/OfflineClient.api.test.ts
@@ -5,6 +5,7 @@ import "cross-fetch/polyfill";
 import { HttpLink } from "apollo-link-http";
 import { OfflineClient, createClient, ApolloOfflineQueueListener } from "../src";
 import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 test("createClient does not throw", async () => {
   const url = "http://test";
@@ -24,6 +25,40 @@ test("OfflineClient constructor does not throw", async () => {
   expect(client.apolloClient).toBeDefined();
   expect(initClient.offlineStore).toBeDefined();
   expect(client.registerOfflineEventListener).toBeDefined();
+});
+
+test("OfflineClient accepts user provided cache", async () => {
+
+  const cache = new InMemoryCache();
+
+  const url = "http://test";
+
+  const offlineClient = new OfflineClient({
+    httpUrl: url,
+    cache
+  });
+
+  const client = await offlineClient.init();
+
+  expect(client).toBeDefined();
+  expect(client.cache).toBe(cache);
+  expect(client instanceof ApolloClient).toBe(true);
+});
+
+test("OfflineClient does not accept non InMemoryCache cache implementation", async () => {
+
+  const cache = { not: "valid" } as unknown as InMemoryCache;
+
+  const url = "http://test";
+
+  function createNewClient() {
+    return new OfflineClient({
+      httpUrl: url,
+      cache
+    });
+  }
+
+  expect(createNewClient).toThrowError("Unsupported cache. cache must be an InMemoryCache");
 });
 
 test("OfflineClient using terminatingLink", async () => {
