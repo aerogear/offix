058ba719dce85f298ab1fa303db992f1e8aafc8b
Dara Hayes <dara.s.hayes@gmail.com>
2019-11-07 10:42:56 +0000

    fix: add comments to OffixOptions

--- packages/offix-scheduler/src/config/OffixOptions.ts
+++ packages/offix-scheduler/src/config/OffixOptions.ts
@@ -5,24 +5,39 @@ import { OffixExecutor } from "../Offix";
 import { OfflineStoreSerializer } from "offix-offline/types/offline/storage/OfflineStoreSerializer";
 
 /**
- * Contains all configuration options required to initialize Voyager Client
- * Options marked with [Modifier] flag are used to modify behavior of client.
- * SDK provides default values for all [Modifier] flags.
- * Users do not need to pass them for normal initialization of the client.
- * Please refer to documentation for more information about the individual flag and it's side effects.
- *
+ * The options that can be passed by a user to initialize the OffixScheduler
+ * Options marked with [Modifier] flag are used to modify behavior of the scheduler
+ * Default values are provided for all [Modifier] flags.
  */
 export interface OffixOptions {
 
+  /**
+   * [Modifier]
+   * 
+   * A class or object with an 'execute' function.
+   * This is the core function you want to be able to schedule while offline.
+   * It could be a HTTP call, A GraphQL request, send a message.
+   */
   executor?: OffixExecutor;
 
   /**
    * [Modifier]
    *
-   * The storage you want your client to use (Uses window.localStorage by default)
+   * The storage you want your client to use (Uses IndexedDB by default)
    */
   storage?: PersistentStore<PersistedData>;
 
+  /**
+   * [Modifier]
+   * 
+   * An object or class that has methods for:
+   * 1. serializing objects from the offline queue to storage.
+   * 2. deserializing objects from storage back to the queue.
+   * 
+   * The objects being serialized/deserialized are based on what's passed to `execute`.
+   * If your objects are simple JSON objects that don't have any functions or circular references
+   * Then you probably don't need a custom serializer.
+   */
   serializer?: OfflineStoreSerializer<any>;
 
   /**
