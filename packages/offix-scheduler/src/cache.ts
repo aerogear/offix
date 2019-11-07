9fa2db4f8101906b7fd96b266de7860fccb44687
Dara Hayes <dara.s.hayes@gmail.com>
2019-11-07 09:27:28 +0000

    fix: remove cache.ts from scheduler

--- packages/offix-scheduler/src/cache.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from "idb-localstorage";
-
-export const createDefaultCacheStorage = () => {
-  try {
-    return new Store("apollo-cache", "cache-store");
-  } catch (error) {
-    console.error("Failed to create storage", error);
-  }
-};
