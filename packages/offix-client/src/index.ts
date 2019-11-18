f93d28624e17d80b4948287ef3220ae16f90980e
Dara Hayes <dara.s.hayes@gmail.com>
2019-11-18 10:48:18 +0000

    fix: export previously exported classes and types

--- packages/offix-client/src/index.ts
+++ packages/offix-client/src/index.ts
@@ -1,4 +1,16 @@
 export * from "./OfflineClient";
 export * from "./apollo/ApolloOfflineClient";
 export * from "offix-offline";
+export * from "offix-conflicts-client";
 export * from "offix-cache";
+
+export * from "./apollo/helpers";
+
+export {
+  PersistentStore,
+  PersistedData,
+  OfflineQueueListener,
+  QueueEntryOperation,
+  QueueEntry,
+  createDefaultOfflineStorage
+} from "offix-scheduler";
