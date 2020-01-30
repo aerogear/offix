653b2ac0f242d578a3f41adf49693578ef766ded
Gianluca <gianlucazuccarelli@gmail.com>
2020-01-30 14:12:11 +0000

    fix: remove old examples & bring react example to examples dir

--- examples/conflicts/index.js
+++
@@ -1,7 +0,0 @@
-const { app, server } = require('./server')
-
-const port = 4000
-app.get('/', (req, res) => res.send('ok'))
-app.listen({ port }, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
-)
--- examples/getting_started/react/config.js
+++
@@ -1,8 +0,0 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-
-export default {
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
-  cache: new InMemoryCache(),
-};
-
