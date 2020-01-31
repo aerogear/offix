915e2e6fd311a7305e8c3a2623313493b8a7495d
Gianluca <gianlucazuccarelli@gmail.com>
2020-01-31 10:53:15 +0000

    feat: add network status react hook

---
+++ packages/react-offix-hooks/src/useNetworkStatus.ts
@@ -0,0 +1,35 @@
+// eslint-disable-next-line
+import React, { useEffect, useState } from "react";
+import { useApolloOfflineClient } from "./ApolloOfflineProvider";
+
+/**
+ * React hook to detect network changes
+ * and return current network state
+ *
+ * Usage: const isOnline = useNetworkStatus();
+ *
+ */
+export function useNetworkStatus(){
+  const client = useApolloOfflineClient();
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    const setOnlineStatus  = async () => {
+      // check if app is offline and return result
+      const offline = await client.networkStatus.isOffline();
+      // set network state with result of offline check
+      setIsOnline(!offline);
+    };
+
+
+    setOnlineStatus();
+
+    // set up network listener to
+    client.networkStatus.onStatusChangeListener({
+      // get result and set online state to result
+      onStatusChange: ({ online }) => setIsOnline(online)
+    });
+  }, []);
+
+  return isOnline;
+};
