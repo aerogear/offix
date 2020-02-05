d16fab5053a0da6681f5104e2c2dec816098a8b0
Dara Hayes <dara.s.hayes@gmail.com>
2020-02-05 10:15:29 +0000

    fix: broken export

--- packages/offix-cache/src/index.ts
+++ packages/offix-cache/src/index.ts
@@ -3,6 +3,5 @@ export * from "./utils/helperFunctions";
 export * from "./createMutationOptions";
 export * from "./createOptimisticResponse";
 export * from "./createSubscriptionOptions";
-export * from "./mutationUpdateFunctions";
 export * from "./api/CacheUpdates";
 export * from "./api/CacheOperation";
