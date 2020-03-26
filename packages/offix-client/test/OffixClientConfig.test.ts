ec1d5adbdd6e374d7cc03b5192c8422692ce396b
Eunovo <eunovo9@gmail.com>
2020-03-26 18:24:59 +0100

    Create config validator tests

--- packages/offix-client/test/OffixClientConfig.test.ts
+++ packages/offix-client/test/OffixClientConfig.test.ts
@@ -1,9 +1,11 @@
 import "fake-indexeddb/auto";
 import "cross-fetch/polyfill";
+import validateConfig from "../src/config/ApolloOfflineClientConfigValidator";
 import { ApolloOfflineClientConfig } from "../src/config/ApolloOfflineClientConfig";
 import { ApolloOfflineClientOptions } from "../src/config/ApolloOfflineClientOptions";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ConfigError } from "../src/config/ConfigError";
 
 test("ApolloOfflineClientConfig Merges defaults with user config", () => {
 
@@ -40,3 +42,23 @@ it("conflict strategy is a function", () => {
   const mergedConfig = new ApolloOfflineClientConfig(configWithStrategy);
   expect(mergedConfig.conflictStrategy).toBe(configWithStrategy.conflictStrategy);
 });
+
+it("Should not throw for a valid config", () => {
+  const link = new HttpLink({ uri: "http://test" });
+
+  const configWithStrategy: ApolloOfflineClientOptions = {
+    // storage,
+    conflictStrategy: {
+      resolve: ({ base, server, client }) => server
+    },
+    cache: new InMemoryCache(),
+    link
+  };
+
+  const config = new ApolloOfflineClientConfig(configWithStrategy);
+  expect(() => validateConfig(config)).not.toThrow(Error);
+});
+
+it("Should throw for invalid config", () => {
+  expect(() => validateConfig({} as any)).toThrow(Error);
+}); 
