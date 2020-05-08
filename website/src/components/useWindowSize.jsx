e8ed0682cd8e69632c2d8da4bded9945bcbad48b
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-08 16:47:24 +0100

    docs: add window size hook

---
+++ website/src/components/useWindowSize.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined
+    };
+  }
+
+  const [windowSize, setWindowSize] = React.useState(getSize);
+
+  React.useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+    
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
