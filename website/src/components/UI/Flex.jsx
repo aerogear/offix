a475673dd34149b5c17a8850e2be42dace2a228e
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-08 16:44:17 +0100

    docs: create ui styled components

---
+++ website/src/components/UI/Flex.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Flex = styled.div`
+  display: flex;
+  height: ${props => props.height};
+  min-height: ${props => props.minHeight};
+  width: ${props => props.width};
+  align-items: ${props => props.alignItems};
+  justify-content: ${props => props.justifyContent};
+  margin: ${props => props.margin};
+  background: ${props => props.background};
+  color: ${props => props.color};
+  order: ${props => props.order};
+`;
+
+Flex.defaultProps = {
+  width: '100%',
+  height: '100vh',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: '0 auto'
+}
