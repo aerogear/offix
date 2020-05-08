4d1a9634042b899f52743ba047d9eba448a76e5e
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-08 17:27:16 +0100

    docs: update features text

--- website/src/components/Features/features.js
+++ website/src/components/Features/features.js
@@ -7,7 +7,9 @@ export const features = [
     imageUrl: 'img/undraw_data_extraction.svg',
     description: (
       <>
-        Mutations are persisted while offline.
+        Offix provides first class support for performing GraphQL 
+        operations while offline. Mutations are held in a queue 
+        that is configured to hold requests while the client is offline. 
       </>
     ),
   },
@@ -17,7 +19,9 @@ export const features = [
     imageUrl: 'img/undraw_yoga.svg',
     description: (
       <>
-        Offline workflows for seamless UI.
+        When the client goes offline for long periods of time, it will 
+        still be able negotiate local updates with the server state thanks 
+        to powerful conflict resolution strategies.
       </>
     ),
   },
@@ -27,7 +31,9 @@ export const features = [
     imageUrl: 'img/undraw_online_connection.svg',
     description: (
       <>
-        Works with React, Angular & Vue.
+        Subscriptions and binary file uploads that work even while the client is offline.
+        Optimistic responses are added to the UI immediately and the mutations are persisted
+        when the client comes back online.
       </>
     ),
   },
@@ -37,7 +43,8 @@ export const features = [
     imageUrl: 'img/undraw_progressive_app.svg',
     description: (
       <>
-        Mutations are persisted while offline.
+        Offix can be easily implemented into native platforms such as React Native, 
+        Ionic and Cordova with minimal amounts of configuration.
       </>
     ),
   },
@@ -47,7 +54,9 @@ export const features = [
     imageUrl: 'img/undraw_code_review.svg',
     description: (
       <>
-        Offline workflows for seamless UI.
+        Offix can be plugged it into the front-end framework of your choosing. 
+        Additional helper methods are provided so Offix can be used through React 
+        with the help of React hooks.
       </>
     ),
   },
@@ -57,7 +66,9 @@ export const features = [
     imageUrl: 'img/undraw_usability_testing.svg',
     description: (
       <>
-        Works with React, Angular & Vue.
+        Offix offers a comprehensive set of features to perform data operations when offline.
+        Leveraging optimistic responses and cache updates under the hood results in a seamless 
+        and friendly user experience.
       </>
     ),
   },
