f6fe1cef32d584a1f5d15b629883aaa806f3ec86
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-18 09:25:23 +0100

    docs: update intro text

--- website/src/components/Introduction/index.jsx
+++ website/src/components/Introduction/index.jsx
@@ -10,7 +10,11 @@ export function Introduction() {
           <Flex height="100%" width="50%" color="#fff">
             <div> 
               <Title>Offline First!</Title>
-              <Paragraph>Offix allows you to execute your GraphQL mutations and queries while your application is offline (or while the server is unreachable). Offline Mutations are scheduled and persisted (across application restarts) and are replayed when server becomes available again.</Paragraph>
+              <Paragraph>
+                Offix allows you to execute your GraphQL mutations and queries while your application is offline, 
+                or while the server is unreachable. Offline Mutations are scheduled and persisted across application restarts, 
+                and are replayed when server becomes available again.
+              </Paragraph>
             </div>
           </Flex>
           <Flex height="100%" width="50%">
