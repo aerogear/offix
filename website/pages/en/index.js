fe33fe4d737bde185cef144c6dc543a81bda5017
Wojciech Trocki <wtrocki@redhat.com>
2020-05-21 23:04:59 +0100

    fix: clean up index file

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -9,7 +9,6 @@ const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
 
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
@@ -103,79 +102,15 @@ class Index extends React.Component {
         <h2>Introduction</h2>
         <div>
           <p>
-            Offix allows you to execute your GraphQL mutations and queries while your application is offline (or while the server is unreachable).
+            Offix allows to execute your GraphQL mutations and queries while your application is offline.
             Offline Mutations are scheduled and persisted (across application restarts) and are replayed when server becomes available again.
           </p>
           <p>
-            We leverage the <a href="https://www.apollographql.com/">Apollo Cache</a>, allowing users to see their local changes and to perform fully offline workflows.
+            Offix leveragegs the <a href="https://www.apollographql.com/">Apollo Cache</a>, allowing users to see their local changes and to perform fully offline workflows.
             Fully featured conflict resolution strategies are provided, as well as interface to build custom ones, ensuring you can build seamless offline experiences.
           </p>
         </div>
       </div>
-    );
-
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations'
-          }
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/undraw_note_list.svg`,
-            imageAlign: 'right',
-            title: 'Description'
-          }
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
-            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
-            imageAlign: 'right',
-            title: 'Randomly Generated Theme Colors'
-          }
-        ]}
-      </Block>
-    );
-
-
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: 'top',
-            title: 'Feature One'
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two'
-          }
-        ]}
-      </Block>
     );
 
     const Intro = () => {
@@ -203,47 +138,12 @@ class Index extends React.Component {
         ]}
       </Block>
     );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
-
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
-          {/**<Features />**/}
           <FeatureCallout />
           <Intro />
-          {/**<LearnHow />**/}
-          {/**<TryOut />**/}
-          {/* <Description /> */}
-          {/* <Showcase /> */}
         </div>
       </div>
     );
