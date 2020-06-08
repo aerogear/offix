f3df972ef46c35cc4762d93866e03bd15f76d4f2
Eunovo <eunovo9@gmail.com>
2020-06-08 18:51:54 +0100

    Handle schema updates in IndexedDB

--- packages/offix-datastore/src/storage/IndexedDBStorage.ts
+++ packages/offix-datastore/src/storage/IndexedDBStorage.ts
@@ -4,25 +4,35 @@ import { Storage } from "./Storage";
 const DB_NAME = "offix-datastore";
 
 export class IndexedDBStorage implements Storage {
-    private indexedDB: Promise<IDBDatabase>;
+    public readonly indexedDB: Promise<IDBDatabase>;
 
-    constructor(models: Model[]) {
+    constructor(models: Model[], schemaVersion: number) {
         this.indexedDB = new Promise((resolve, reject) => {
-            const openreq = indexedDB.open(DB_NAME, 1);
+            const openreq = indexedDB.open(DB_NAME, schemaVersion);
             openreq.onerror = () => reject(openreq.error);
             openreq.onsuccess = () => {
                 const db = openreq.result;
                 db.onversionchange = function () {
-                    db.close();
-                    alert("Please reload the page.")
+                    this.close();
+                    alert("Please reload the page.");
                 };
-
                 resolve(db);
             };
 
             openreq.onupgradeneeded = () => {
+                const db = openreq.result;
+                const storeNames = db.objectStoreNames;
+
+                for (let i = 0; i < storeNames.length; i++) {
+                    const storeName = (storeNames.item(i) as string);
+                    if (models.find((value) => value.storeName === storeName)) return;
+
+                    // model has been removed, remove it's store
+                    db.deleteObjectStore(storeName);
+                }
                 models.forEach(({ storeName }: Model) => {
-                    openreq.result.createObjectStore(storeName);
+                    if (storeNames.contains(storeName)) return;
+                    db.createObjectStore(storeName);
                 });
             };
         });
--- packages/offix-datastore/src/storage/defaultStorage.ts
+++ packages/offix-datastore/src/storage/defaultStorage.ts
@@ -1,6 +1,6 @@
 import { IndexedDBStorage } from "./IndexedDBStorage";
 import { Model } from "../models";
 
-export function createDefaultStorage(models: Model[]) {
-    return new IndexedDBStorage(models);
+export function createDefaultStorage(models: Model[], schemaVersion: number) {
+    return new IndexedDBStorage(models, schemaVersion);
 }
