150ad1c3a315af4fd520bda50c8e7d22e5197a7d
Eunovo <eunovo9@gmail.com>
2020-06-15 13:48:16 +0100

    Implement observe store for changes functionality

---
+++ packages/offix-datastore/src/utils/PushStream.ts
@@ -0,0 +1,31 @@
+import Observable from "zen-observable";
+
+export interface Subscription {
+    unsubscribe(): void;
+}
+
+export interface PushStream<T> {
+    push(message: T): void;
+    subscribe(listener: (message: T) => void): Subscription;
+}
+
+export class ObservablePushStream<T> implements PushStream<T> {
+    private observable: Observable<T>;
+    private observer: any;
+
+    constructor() {
+        this.observable = new Observable(observer => {
+            this.observer = observer;
+        });
+        // disable @typescript-eslint/no-empty-function
+        this.observable.subscribe((e) => {});
+    }
+
+    public push(message: T) {
+        this.observer.next(message);
+    }
+
+    public subscribe(listener: (message: T) => void) {
+        return (this.observable.subscribe(listener) as Subscription);
+    }
+}
