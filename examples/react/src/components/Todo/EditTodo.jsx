b37db62385a6fdfe085bab8a22b879d12fa76013
Eunovo <eunovo9@gmail.com>
2020-06-29 13:22:30 +0100

    Use offix-datastore in example react app

--- examples/react/src/components/Todo/EditTodo.jsx
+++ examples/react/src/components/Todo/EditTodo.jsx
@@ -1,62 +1,86 @@
-import React, { useRef } from 'react';
+import React, { useRef } from 'react'
 
 export const EditTodo = ({ todo, editTodo, toggleEdit }) => {
-  const titleRef = useRef();
-  const descRef = useRef();
+  const titleRef = useRef()
+  const descRef = useRef()
 
   const getInputs = () => {
-    const title = (titleRef.current.value === '') ? todo.title : titleRef.current.value;
-    const description = (descRef.current.value === '') ? todo.description : descRef.current.value;
+    const title =
+      titleRef.current.value === '' ? todo.title : titleRef.current.value
+    const description =
+      descRef.current.value === '' ? todo.description : descRef.current.value
     // return an array of inputs
-    return [title, description];
-  };
+    return [title, description]
+  }
 
   const clear = () => {
-    titleRef.current.value = '';
-    descRef.current.value = '';
-    toggleEdit();
-  };
+    titleRef.current.value = ''
+    descRef.current.value = ''
+    toggleEdit()
+  }
 
-  const handleUpdate = (e) => {
-    e.preventDefault();
+  const handleUpdate = e => {
+    e.preventDefault()
 
     // get serialized inputs
-    const [title, description] = getInputs();
+    const [title, description] = getInputs()
 
     // execute mutation
     editTodo({
-      variables: {
-        ...todo,
-        title,
-        description,
-      },
-    });
+      title,
+      description
+    }, (t) => t.id("eq", todo.id))
 
-    clear();
-  };
+    clear()
+  }
 
   return (
-    <div className="columns">
-      <div className="column col-12">
+    <div className='columns'>
+      <div className='column col-12'>
         <form onSubmit={handleUpdate}>
-          <div className="form-group">
-            <label className="form-label" htmlFor="title-edit">Title</label>
-            <input id="title-edit" name="title" type="text" className="form-input" ref={titleRef} placeholder={todo.title} />
+          <div className='form-group'>
+            <label className='form-label' htmlFor='title-edit'>
+              Title
+            </label>
+            <input
+              id='title-edit'
+              name='title'
+              type='text'
+              className='form-input'
+              ref={titleRef}
+              placeholder={todo.title}
+            />
           </div>
-          <div className="form-group">
-            <label className="form-label" htmlFor="desc-edit">Description</label>
-            <textarea id="desc-edit" name="description" className="form-input mb-4" ref={descRef} placeholder={todo.description} />
+          <div className='form-group'>
+            <label className='form-label' htmlFor='desc-edit'>
+              Description
+            </label>
+            <textarea
+              id='desc-edit'
+              name='description'
+              className='form-input mb-4'
+              ref={descRef}
+              placeholder={todo.description}
+            />
           </div>
-          <div className="btn-group float-right">
-            <button type="submit" className="btn btn-sm btn-primary" onClick={handleUpdate}>
+          <div className='btn-group float-right'>
+            <button
+              type='submit'
+              className='btn btn-sm btn-primary'
+              onClick={handleUpdate}
+            >
               Edit
             </button>
-            <button type="button" className="btn btn-sm btn-error" onClick={clear}>
+            <button
+              type='button'
+              className='btn btn-sm btn-error'
+              onClick={clear}
+            >
               Cancel
             </button>
           </div>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,37 +1,5 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import { createClient } from "offix-client-boost";
-import { ApolloOfflineProvider } from "react-offix-hooks";
-import { ApolloProvider } from "@apollo/react-hooks";
-import App from "./App";
-import { Loading } from "./components";
-import { clientConfig } from "./clientConfig";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-let client = undefined;
-
-const Main = () => {
-  const [initialized, setInitialized] = useState(false);
-
-  useEffect(() => {
-    createClient(clientConfig).then((newClient) => {
-      client = newClient;
-      setInitialized(true);
-    }).catch((e)=>{
-      console.log(e);
-    });
-  }, []);
-
-  // If client is still initializing,
-  // display loading screen
-  if (!initialized) return <Loading />;
-
-  return (
-    <ApolloOfflineProvider client={client}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </ApolloOfflineProvider>
-  );
-};
-
-ReactDOM.render(<Main />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
