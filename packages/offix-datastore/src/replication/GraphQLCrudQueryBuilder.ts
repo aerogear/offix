2f5622151734bd6f52222e5edf2daaee9471d87c
Wojciech Trocki <wtrocki@redhat.com>
2020-07-08 10:50:24 +0100

    fix: remove type that was missing and add some TODO's

--- packages/offix-datastore/src/replication/GraphQLCrudQueryBuilder.ts
+++ packages/offix-datastore/src/replication/GraphQLCrudQueryBuilder.ts
@@ -7,6 +7,8 @@ import { Model } from "../Model";
  * Builds GraphQL queries for models following
  * the [GraphQLCRUD specification]{@link https://graphqlcrud.org/}
  */
+// TODO add queries
+// TODO have static/precompiled gql queries instead of processing all at runtime
 export class GraphQLCrudQueryBuilder implements GraphQLQueryBuilder {
     build(models: Model<any>[]): Map<string, GraphQLQueries> {
         const queriesMap: Map<string, GraphQLQueries> = new Map();
