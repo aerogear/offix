0ffe43b8f38e49aa6d8b3475fabe9759a38681db
Wojciech Trocki <wtrocki@redhat.com>
2020-07-08 10:52:24 +0100

    fix: unit tests linting issues

--- packages/offix-datastore/tests/DataStore.test.ts
+++ packages/offix-datastore/tests/DataStore.test.ts
@@ -14,152 +14,152 @@ import { Predicate } from "../src/predicates";
 const DB_NAME = "offix-datastore";
 
 function getIndexedDB() {
-    return new Promise<IDBDatabase>((resolve, reject) => {
-        const request = window.indexedDB.open(DB_NAME, 1);
-        request.onerror = () => reject(request.error);
-        request.onsuccess = () => resolve(request.result);
-    });
+  return new Promise<IDBDatabase>((resolve, reject) => {
+    const request = window.indexedDB.open(DB_NAME, 1);
+    request.onerror = () => reject(request.error);
+    request.onsuccess = () => resolve(request.result);
+  });
 }
 
 interface Note {
-    id?: string;
-    title: string;
-    description: string;
+  id?: string;
+  title: string;
+  description: string;
 }
 
 interface Comment {
-    id?: string;
-    title: string;
-    noteId: string;
+  id?: string;
+  title: string;
+  noteId: string;
 }
 
 let NoteModel: Model<Note>;
 
 beforeEach(() => {
-    const dataStore = new DataStore({
-        dbName: DB_NAME,
-        url: "http://localhost:4000/"
-    });
-    NoteModel = dataStore.create<Note>("Note", "user_Note", {
-        id: {
-            type: "ID",
-            key: "id"
-        },
-        title: {
-            type: "String",
-            key: "title"
-        },
-        description: {
-            type: "String",
-            key: "description"
-        }
-    });
-    dataStore.create<Comment>("Comment", "user_Comment", {
-        id: {
-            type: "ID",
-            key: "id"
-        },
-        title: {
-            type: "String",
-            key: "title"
-        },
-        noteId: {
-            type: "ID",
-            key: "noteId"
-        }
-    });
-    dataStore.init();
+  const dataStore = new DataStore({
+    dbName: DB_NAME,
+    url: "http://localhost:4000/"
+  });
+  NoteModel = dataStore.create<Note>("Note", "user_Note", {
+    id: {
+      type: "ID",
+      key: "id"
+    },
+    title: {
+      type: "String",
+      key: "title"
+    },
+    description: {
+      type: "String",
+      key: "description"
+    }
+  });
+  dataStore.create<Comment>("Comment", "user_Comment", {
+    id: {
+      type: "ID",
+      key: "id"
+    },
+    title: {
+      type: "String",
+      key: "title"
+    },
+    noteId: {
+      type: "ID",
+      key: "noteId"
+    }
+  });
+  dataStore.init();
 });
 
 afterEach(async () => {
-    await new Promise((resolve, reject) => {
-        const del = indexedDB.deleteDatabase(DB_NAME);
-        del.onsuccess = event => resolve();
-        del.onblocked = event => resolve();
-    });
+  await new Promise((resolve, reject) => {
+    const del = indexedDB.deleteDatabase(DB_NAME);
+    del.onsuccess = event => resolve();
+    del.onblocked = event => resolve();
+  });
 });
 
 test("Setup client db with provided models", async () => {
-    const db = await getIndexedDB();
-    expect(db.objectStoreNames).toContain("user_Note");
-    expect(db.objectStoreNames).toContain("user_Comment");
-    db.close();
+  const db = await getIndexedDB();
+  expect(db.objectStoreNames).toContain("user_Note");
+  expect(db.objectStoreNames).toContain("user_Comment");
+  db.close();
 });
 
 test("Store Schema update", async () => {
-    const idbStorage = createDefaultStorage(DB_NAME, [
-        new Model("Test", "user_Test", {},  () => (null as any))
-    ], 2);
-    const db = await idbStorage.getIndexedDBInstance();
-    expect(db.objectStoreNames).toContain("user_Test");
-    expect(db.objectStoreNames).not.toContain("user_Note");
-    expect(db.objectStoreNames).not.toContain("user_Comment");
+  const idbStorage = createDefaultStorage(DB_NAME, [
+    new Model("Test", "user_Test", {}, () => (null as any))
+  ], 2);
+  const db = await idbStorage.getIndexedDBInstance();
+  expect(db.objectStoreNames).toContain("user_Test");
+  expect(db.objectStoreNames).not.toContain("user_Note");
+  expect(db.objectStoreNames).not.toContain("user_Comment");
 });
 
 test("Save Note to local store", async () => {
-    const note = { title: "test", description: "description" };
-    const savedNote = await NoteModel.save(note);
-    expect(savedNote.id).toBeDefined();
-    expect(savedNote.title).toBe(note.title);
+  const note = { title: "test", description: "description" };
+  const savedNote = await NoteModel.save(note);
+  expect(savedNote.id).toBeDefined();
+  expect(savedNote.title).toBe(note.title);
 });
 
 test("Query from local store", async () => {
-    const note = { title: "test", description: "description" };
-    const savedNote = await NoteModel.save(note);
-    const results = (await NoteModel.query((p) => p.title("eq", note.title)));
-    expect(results[0]).toHaveProperty("id", savedNote.id);
+  const note = { title: "test", description: "description" };
+  const savedNote = await NoteModel.save(note);
+  const results = (await NoteModel.query((p) => p.title("eq", note.title)));
+  expect(results[0]).toHaveProperty("id", savedNote.id);
 });
 
 test("Update single entity in local store", async () => {
-    const note = { title: "test", description: "description" };
-    const savedNote = await NoteModel.save(note);
-    const newTitle = "updated note";
+  const note = { title: "test", description: "description" };
+  const savedNote = await NoteModel.save(note);
+  const newTitle = "updated note";
 
-    await NoteModel.update({
-        ...savedNote,
-        title: newTitle
-    }, (p) => p.id("eq", savedNote.id));
+  await NoteModel.update({
+    ...savedNote,
+    title: newTitle
+  }, (p) => p.id("eq", savedNote.id));
 
-    const updatedNote = (await NoteModel.query((p) => p.id("eq", savedNote.id)))[0];
+  const updatedNote = (await NoteModel.query((p) => p.id("eq", savedNote.id)))[0];
 
-    expect(updatedNote.title).toEqual(newTitle);
+  expect(updatedNote.title).toEqual(newTitle);
 });
 
 test("Remove single entity from local store", async () => {
-    const note = { title: "test", description: "description" };
-    const savedNote = await NoteModel.save(note);
-    await NoteModel.remove();
-    const results = (await NoteModel.query((p) => p.id("eq", savedNote.id)));
-    expect(results.length).toEqual(0);
+  const note = { title: "test", description: "description" };
+  const savedNote = await NoteModel.save(note);
+  await NoteModel.remove();
+  const results = (await NoteModel.query((p) => p.id("eq", savedNote.id)));
+  expect(results.length).toEqual(0);
 });
 
 test("Remove all entities matching predicate from local store", async () => {
-    const note = { title: "test", description: "description" };
-    const savedNote = await NoteModel.save(note);
-    const predicate: Predicate<Note> = (p) => p.id("eq", savedNote.id);
-    await NoteModel.remove(predicate);
-    const results = (await NoteModel.query(predicate));
-    expect(results.length).toEqual(0);
+  const note = { title: "test", description: "description" };
+  const savedNote = await NoteModel.save(note);
+  const predicate: Predicate<Note> = (p) => p.id("eq", savedNote.id);
+  await NoteModel.remove(predicate);
+  const results = (await NoteModel.query(predicate));
+  expect(results.length).toEqual(0);
 });
 
 test("Observe local store events", async () => {
-    const note = { title: "test", description: "description" };
-    expect.assertions(3);
+  const note = { title: "test", description: "description" };
+  expect.assertions(3);
 
-    NoteModel.on("ADD", (event) => {
-        expect(event.eventType).toEqual("ADD");
-        expect(event.data.title).toEqual(note.title);
-    });
-    NoteModel.on("UPDATE", (event) => {
-        expect(event.eventType).toEqual("UPDATE");
-    });
+  NoteModel.on("ADD", (event) => {
+    expect(event.eventType).toEqual("ADD");
+    expect(event.data.title).toEqual(note.title);
+  });
+  NoteModel.on("UPDATE", (event) => {
+    expect(event.eventType).toEqual("UPDATE");
+  });
 
-    await NoteModel.save(note);
-    await NoteModel.update({ title: "changed" }, (p) => p.title("eq", "test"));
+  await NoteModel.save(note);
+  await NoteModel.update({ title: "changed" }, (p) => p.title("eq", "test"));
 });
 
 test("Push local change to server", (done) => {
-    const typeDefs = `
+  const typeDefs = `
     type Note {
         id: ID
         title: String
@@ -180,23 +180,23 @@ test("Push local change to server", (done) => {
         createNote(input: CreateNoteInput!): Note
     }
     `;
-    const note: Note = {
-        title: "test",
-        description: "test"
-    };
+  const note: Note = {
+    title: "test",
+    description: "test"
+  };
 
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers: {
-            Mutation: {
-                createNote: (_, { input }) => {
-                    expect(input).toHaveProperty("title", note.title);
-                    expect(input).toHaveProperty("description", note.description);
-                    server.stop().finally(() => done());
-                }                
-            }
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+      Mutation: {
+        createNote: (_, { input }) => {
+          expect(input).toHaveProperty("title", note.title);
+          expect(input).toHaveProperty("description", note.description);
+          server.stop().finally(() => done());
         }
-    });
-    server.listen();
-    NoteModel.save(note);
+      }
+    }
+  });
+  server.listen();
+  NoteModel.save(note);
 });
