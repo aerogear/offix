47698af9f04f9ed7292dfcb6b2a1991c34064a1d
Wojciech Trocki <wtrocki@redhat.com>
2020-07-08 10:52:24 +0100

    fix: unit tests linting issues

--- packages/offix-datastore/tests/GraphQLReplication.test.ts
+++ packages/offix-datastore/tests/GraphQLReplication.test.ts
@@ -7,41 +7,41 @@ let queries: Map<string, GraphQLQueries>;
 let modelQueries: GraphQLQueries;
 
 beforeAll(() => {
-    const dataStore = new DataStore({ dbName: "test", url: "" });
-    model = dataStore.create<any>("Test", "test", {
-        id: {
-            type: "ID",
-            key: "id"
-        },
-        title: {
-            type: "String",
-            key: "title"
-        }
-    });
-    const queryBuilder = new GraphQLCrudQueryBuilder();
-    queries = queryBuilder.build([model]);
-    modelQueries = (queries.get(model.getStoreName()) as GraphQLQueries);
+  const dataStore = new DataStore({ dbName: "test", url: "" });
+  model = dataStore.create<any>("Test", "test", {
+    id: {
+      type: "ID",
+      key: "id"
+    },
+    title: {
+      type: "String",
+      key: "title"
+    }
+  });
+  const queryBuilder = new GraphQLCrudQueryBuilder();
+  queries = queryBuilder.build([model]);
+  modelQueries = (queries.get(model.getStoreName()) as GraphQLQueries);
 });
 
 test("Test Query generation", () => {
-    expect(modelQueries).toMatchSnapshot();
+  expect(modelQueries).toMatchSnapshot();
 });
 
 test("Push mutation to GraphQL Server", (done) => {
-    const input = { title: "test" };
+  const input = { title: "test" };
 
-    const graphQLReplicaionAPI = new GraphQLReplicator({
-        mutate: async (query, variables: any) => {
-            expect(query).toEqual(modelQueries.mutations.create);
-            expect(variables.input).toEqual(input);
-            done();
-            return { data: null, errors: [] }
-        }
-    }, queries);
+  const graphQLReplicaionAPI = new GraphQLReplicator({
+    mutate: async (query, variables: any) => {
+      expect(query).toEqual(modelQueries.mutations.create);
+      expect(variables.input).toEqual(input);
+      done();
+      return { data: null, errors: [] };
+    }
+  }, queries);
 
-    graphQLReplicaionAPI.push({
-        eventType: "ADD",
-        input,
-        storeName: model.getStoreName()
-    })
+  graphQLReplicaionAPI.push({
+    eventType: "ADD",
+    input,
+    storeName: model.getStoreName()
+  });
 });
