fcfaddc2e6ca6ed3e75202d56d008af28b1b1eeb
Gianluca <gianlucazuccarelli@gmail.com>
2020-07-14 16:46:59 +0100

    chore: setup subscriptions

--- packages/offix-datastore/src/replication/UrqlGraphQLClient.ts
+++ packages/offix-datastore/src/replication/UrqlGraphQLClient.ts
@@ -1,13 +1,28 @@
-import { Client, createClient } from "urql";
+import { Client, createClient, subscriptionExchange, defaultExchanges } from "urql";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import { DocumentNode } from "graphql";
+import { pipe, subscribe } from "wonka";
 
 import { GraphQLClient } from "./GraphQLReplicator";
 
 export class UrqlGraphQLClient implements GraphQLClient {
     private client: Client;
 
-    constructor(url: string) {
-        this.client = createClient({ url });
+    constructor(url: string, wsUrl: string | undefined) {
+        if (!wsUrl) {
+          this.client = createClient({ url });
+        } else {
+          const subscriptionClient = new SubscriptionClient(wsUrl, { reconnect: true });
+          const exchanges = [
+            ...defaultExchanges,
+            subscriptionExchange({
+              forwardSubscription: operation => {
+                return subscriptionClient.request(operation);
+              }
+            })
+          ];
+          this.client = createClient({ url, exchanges });
+        }
     }
 
     async query(query: string | DocumentNode, variables?: any) {
@@ -32,5 +47,17 @@ export class UrqlGraphQLClient implements GraphQLClient {
         } catch (error) {
             throw error;
         }
+    }
+
+    public subscribe(query: DocumentNode) {
+      return pipe(
+        this.client.subscription(query),
+        subscribe(result => {
+          return {
+            data: result.data,
+            errors: [result.error] // TODO define proper errors for error handling
+          };
+        })
+      );
     }
 }
