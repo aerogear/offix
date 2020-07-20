9f7e3c5c8557e84257cc81fc8d91b9085be58148
Gianluca <gianlucazuccarelli@gmail.com>
2020-07-20 09:58:36 +0100

    chore: reapply changes from #617

--- packages/offix-datastore/src/Model.ts
+++ packages/offix-datastore/src/Model.ts
@@ -172,15 +172,19 @@ export class Model<T = unknown> {
     if (data.data && data.data.length > 0) {
       data.data
         .filter((d: any) => (d._deleted))
-        .forEach((d: any) => { this.remove(matcher(d)); });
+        .forEach((d: any) => {
+          this.storage.remove(this.storeName,
+            (predicate ? predicate(modelPredicate) : undefined), "replication");
+        });
+
       data.data
         .filter((d: any) => (!d._deleted))
         .forEach((d: any) => {
           (async () => {
-            const results = await this.update(d, matcher(d));
+            const results = await this.storage.update(this.storeName, d, undefined, "replication");
             if (results.length === 0) {
               // no update was made, save the data instead
-              this.save(d);
+              await this.storage.save(this.storeName, d, "replication");
               return;
             }
           })();
