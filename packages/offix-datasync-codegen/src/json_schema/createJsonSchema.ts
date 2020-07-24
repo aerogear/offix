071a2e45e20a476663d2850d0cff55676f8e6bf2
Eunovo <eunovo9@gmail.com>
2020-07-24 15:40:54 +0100

    Map graphql types to ts types

--- packages/offix-datasync-codegen/src/json_schema/createJsonSchema.ts
+++ packages/offix-datasync-codegen/src/json_schema/createJsonSchema.ts
@@ -1,15 +1,37 @@
 import { ModelDefinition } from "@graphback/core";
+import {
+    isNonNullType,
+    GraphQLOutputType,
+    getNullableType,
+} from "graphql";
+import { convertToTsType } from "../utils";
+
+
+const getFieldParameters = (type: GraphQLOutputType): any => {
+    let options: any = {};
+
+    // TODO handle relationships
+
+    if (isNonNullType(type)) {
+        type = getNullableType(type);
+        options = { isRequired: true }
+    }
+    return { type: convertToTsType(type), ...options };
+}
+
+const getModelProperties = (model: ModelDefinition) => {
+    const fieldMap = model.graphqlType.getFields();
+
+    return Object.keys(fieldMap)
+        .map(fieldName => ({
+            [fieldName]: getFieldParameters(fieldMap[fieldName].type)
+        }))
+        .reduce((prev, current) => ({ ...prev, ...current }), {});
+}
 
 export const createJsonSchema = (model: ModelDefinition) => {
     return {
         name: model.graphqlType.name,
         properties: getModelProperties(model)
     }
-}
-
-const getModelProperties = (model: ModelDefinition) => {
-    const fieldMap = model.graphqlType.getFields();
-    return Object.keys(fieldMap)
-        .map(fieldName => ({ [fieldName]: { test: "test" } }))
-        .reduce((prev, current) => ({ ...prev, ...current }), {});
 }
