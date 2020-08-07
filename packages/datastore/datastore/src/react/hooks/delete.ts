1b3a02172c5ed7de13019856a32e305c7cb1ad54
Eunovo <eunovo9@gmail.com>
2020-08-07 17:13:54 +0100

    Use filters in React hooks

--- packages/datastore/datastore/src/react/hooks/delete.ts
+++ packages/datastore/datastore/src/react/hooks/delete.ts
@@ -1,17 +1,17 @@
 import { useReducer } from "react";
 import { Model } from "../../Model";
 import { reducer, InitialState, ActionType } from "../ReducerUtils";
-import { Predicate } from "../../predicates";
+import { Filter } from "../../filters";
 
 export const useRemove = <T>(model: Model<T>) => {
     const [state, dispatch] = useReducer(reducer, InitialState);
 
-    const remove = async (predicate?: Predicate<T>) => {
+    const remove = async (filter?: Filter<T>) => {
         if (state.isLoading) { return; }
 
         dispatch({ type: ActionType.INITIATE_REQUEST });
         try {
-            const results = await model.remove(predicate);
+            const results = await model.remove(filter);
             dispatch({ type: ActionType.REQUEST_COMPLETE, data: results });
         } catch (error) {
             dispatch({ type: ActionType.REQUEST_COMPLETE, error });
--- packages/datastore/datastore/src/react/hooks/update.ts
+++ packages/datastore/datastore/src/react/hooks/update.ts
@@ -1,17 +1,17 @@
 import { useReducer } from "react";
 import { Model } from "../../Model";
 import { reducer, InitialState, ActionType } from "../ReducerUtils";
-import { Predicate } from "../../predicates";
+import { Filter } from "../../filters";
 
 export const useUpdate = <T>(model: Model<T>) => {
     const [state, dispatch] = useReducer(reducer, InitialState);
 
-    const update = async (input: any, predicate?: Predicate<T>) => {
+    const update = async (input: any, filter?: Filter<T>) => {
         if (state.isLoading) { return; }
 
         dispatch({ type: ActionType.INITIATE_REQUEST });
         try {
-            const results = await model.update(input, predicate);
+            const results = await model.update(input, filter);
             dispatch({ type: ActionType.REQUEST_COMPLETE, data: results });
         } catch (error) {
             dispatch({ type: ActionType.REQUEST_COMPLETE, error });
