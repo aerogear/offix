f2216bf29a96c8acb64ccff596e15b6dca5dd9b4
Eunovo <eunovo9@gmail.com>
2020-08-07 16:19:30 +0100

    Implement default filter

--- packages/datastore/datastore/src/storage/adapters/indexedDB/createNodes.ts
+++ packages/datastore/datastore/src/storage/adapters/indexedDB/createNodes.ts
@@ -3,16 +3,20 @@ import { INode, ORNode, NotNode, ANDNode, LeafNode } from "./Nodes";
 export const createNodes = (filter: any): INode[] => {
     return Object.keys(filter)
         .map((fieldKey) => {
+            const value = filter[fieldKey];
             if (fieldKey === "or") {
-                return new ORNode(createNodes(filter[fieldKey]));
+                return new ORNode(createNodes(value));
             }
             if (fieldKey === "not") {
-                return new NotNode(new ANDNode(createNodes(filter[fieldKey])));
+                return new NotNode(new ANDNode(createNodes(value)));
             }
             if (fieldKey === "and") {
-                return new ANDNode(createNodes(filter[fieldKey]));
+                return new ANDNode(createNodes(value));
             }
-
-            return new LeafNode(fieldKey, filter[fieldKey]);
+            if (value instanceof Object) {
+                return new LeafNode(fieldKey, value);
+            }
+            const defaultFilter = { eq: value }
+            return new LeafNode(fieldKey, defaultFilter);
         });
 };
