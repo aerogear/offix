ef7fec8fe495cf7d6b37524051225f39d3308f68
Eunovo <eunovo9@gmail.com>
2020-08-07 13:11:27 +0100

    Describe indexedb filter tests

--- packages/offix-datastore/tests/Filters.test.ts
+++ packages/offix-datastore/tests/Filters.test.ts
@@ -1,29 +1,31 @@
 import { createPredicateFrom } from '../src/storage/adapters/indexedDB/Predicate';
 
-test("Filter based on object fields", () => {
-    const list = [
-        { clickCount: 9 }, { clickCount: 4 }
-    ];
-    const predicate = createPredicateFrom({ clickCount: { lt: 9, ne: 4 } });
-    const result = predicate.filter(list);
-    expect(result.length).toEqual(0);
-});
-
-test("Filter based on expressions", () => {
-    const list = [
-        { clickCount: 9, isTest: true },
-        { clickCount: 4, isTest: false },
-        { clickCount: 4, isTest: true }
-    ];
-    const predicate = createPredicateFrom({
-        or: {
-            clickCount: { eq: 9 },
-            not: {
-                and: { isTest: { eq: false }, clickCount: { eq: 4 } }
-            }
-        }
+describe("Test IndexedDB filters", () => {
+    test("Filter based on object fields", () => {
+        const list = [
+            { clickCount: 9 }, { clickCount: 4 }
+        ];
+        const predicate = createPredicateFrom({ clickCount: { lt: 9, ne: 4 } });
+        const result = predicate.filter(list);
+        expect(result.length).toEqual(0);
     });
-
-    const result = predicate.filter(list);
-    expect(result).toEqual([list[0], list[2]]);
+    
+    test("Filter based on expressions", () => {
+        const list = [
+            { clickCount: 9, isTest: true },
+            { clickCount: 4, isTest: false },
+            { clickCount: 4, isTest: true }
+        ];
+        const predicate = createPredicateFrom({
+            or: {
+                clickCount: { eq: 9 },
+                not: {
+                    and: { isTest: { eq: false }, clickCount: { eq: 4 } }
+                }
+            }
+        });
+    
+        const result = predicate.filter(list);
+        expect(result).toEqual([list[0], list[2]]);
+    });
 });
