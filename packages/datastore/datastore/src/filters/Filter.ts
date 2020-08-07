2efd8477ab6fec0d71444a2f27251bbc07d505fe
Eunovo <eunovo9@gmail.com>
2020-08-07 16:54:29 +0100

    Remove test type

--- packages/datastore/datastore/src/filters/Filter.ts
+++ packages/datastore/datastore/src/filters/Filter.ts
@@ -1,9 +1,5 @@
 import { TypeOperatorMap } from "./Operators";
 
-type Test = {
-    eq: string
-}
-
 export type Filter<T = any> = {
     [P in keyof Partial<T>]: T[P] | { [K in keyof Partial<TypeOperatorMap<T[P]>>]: T[P] }
 } & {
