48509ebe2a7f59bdfe18e3d50ef0badcc36c6bc0
Gianluca <gzuccare@redhat.com>
2020-08-19 09:59:19 +0100

    docs: fix broken links (#738)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       items: [
         {
-          to: 'docs/gettingstarted',
+          to: 'docs/getting-started',
           activeBasePath: 'docs',
           label: 'Docs',
           position: 'left',
