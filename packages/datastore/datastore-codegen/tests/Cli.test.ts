26390e8e5b0e3db45bc32a1c30fe2e7dd7c5370e
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-08-19 15:05:46 +0100

    Test generated type

--- packages/datastore/datastore-codegen/tests/Cli.test.ts
+++ packages/datastore/datastore-codegen/tests/Cli.test.ts
@@ -11,11 +11,11 @@ test("generate", async () => {
     const schemaPath = join(__dirname, './mock.graphql');
 
     await exec(`node ${cliExec} generate --schema ${schemaPath} --outputPath output/`);
-    const schema = readFileSync("./output/schema/schema.json");
-    const schemaIndex = readFileSync("./output/schema/index.ts");
-    const config = readFileSync("./output/config.ts");
+    const schema = readFileSync("./output/schema/schema.json").toString();
+    const schemaIndex = readFileSync("./output/schema/index.ts").toString();
+    const types = readFileSync("./output/types.ts").toString();
 
-    expect(config).toBeDefined();
-    expect(schema).toBeDefined();
-    expect(schemaIndex).toBeDefined();
+    expect(types).toMatchSnapshot();
+    expect(schema).toMatchSnapshot();
+    expect(schemaIndex).toMatchSnapshot();
 });
