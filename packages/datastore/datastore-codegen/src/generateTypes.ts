58b96a776364a23e6ac68105832155b4d5bc82d1
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-08-19 15:39:50 +0100

    Detect primary key

--- packages/datastore/datastore-codegen/src/generateTypes.ts
+++ packages/datastore/datastore-codegen/src/generateTypes.ts
@@ -1,6 +1,21 @@
 import { ModelDefinition } from "@graphback/core";
-import { GraphQLOutputType, isNonNullType, getNullableType } from "graphql";
+import { GraphQLOutputType, isNonNullType, getNullableType, isCompositeType } from "graphql";
 import { convertToTsType } from "./utils";
+
+const isPrimaryKey = (fieldName: string, type: GraphQLOutputType) => {
+    if (isNonNullType(type)) {
+        type = getNullableType(type);
+    }
+
+    return (type.toString() === "ID");
+}
+
+const getPrimaryKey = (model: ModelDefinition) => {
+    const fieldMap = model.graphqlType.getFields();
+
+    return Object.keys(fieldMap)
+        .find((fieldName) => (isPrimaryKey(fieldName, fieldMap[fieldName].type)))
+}
 
 const getField = (fieldName: string, type: GraphQLOutputType) => {
     if (isNonNullType(type)) {
@@ -22,11 +37,13 @@ const getModelProperties = (model: ModelDefinition) => {
 
 export const createModelType = (model: ModelDefinition) => {
     const modelName = model.graphqlType.name;
+    const primaryKey = getPrimaryKey(model);
+
     return `export interface ${modelName} {
     ${getModelProperties(model)}
 }
 
-export type ${modelName}Create = Omit<${modelName}, "id">;
-export type ${modelName}Change = Pick<${modelName}, "id"> && Partial<${modelName}Create>;
+export type ${modelName}Create = ${primaryKey ? `Omit<${modelName}, "${primaryKey}">` : modelName};
+export type ${modelName}Change =  ${primaryKey ? `Pick<${modelName}, "id"> && ` : ""}Partial<${modelName}Create>;
 `;
 }
