d6f83e336d644f78ca53f139c8a6742408ffdf48
Gianluca <gianlucazuccarelli@gmail.com>
2020-08-20 14:59:46 +0100

    chore: update exports

--- packages/datastore/datastore/src/storage/index.ts
+++ packages/datastore/datastore/src/storage/index.ts
@@ -2,5 +2,6 @@ export * from "./LocalStorage";
 export * from "./api/CRUDEvents";
 export * from "./api/StorageAdapter";
 export * from "./api/StoreChangeEvent";
-export { WebSQLAdapter } from "./adapters/websql/WebSQLAdapter";
+export { WebSQLAdapter } from "./adapters/sqlite/WebSQLAdapter";
+export { SQLiteAdapter } from "./adapters/sqlite/SQLiteAdapter";
 
