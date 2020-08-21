50e8ac46e80133026ea44a023e0c125685b983c7
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-08-21 15:22:47 +0100

    Implement default `subscribeToMore` behaviour

--- packages/datastore/datastore/src/react/hooks/query.ts
+++ packages/datastore/datastore/src/react/hooks/query.ts
@@ -1,27 +1,73 @@
 import { useEffect, useReducer, useCallback, Dispatch } from "react";
 import { Model } from "../../Model";
-import { reducer, InitialState, ActionType, Action } from "../ReducerUtils";
-import { CRUDEvents } from "../../storage";
+import { reducer, InitialState, ActionType, Action, ResultState } from "../ReducerUtils";
+import { CRUDEvents, StoreChangeEvent } from "../../storage";
 import { Filter } from "../../filters";
 
-const createSubscribeToMore = (model: Model, dispatch: Dispatch<Action>) => {
-    return (eventType: CRUDEvents, updateResult: (data: any) => any, filter?: Filter) => {
-        // TODO subscribe to specific predicate
+const onAdded = (currentData: any[], newData: any[]) => {
+    if (!currentData) {return newData;}
+    return [...currentData, ...newData];
+};
+
+const onChanged = (currentData: any[], newData: any[]) => {
+    if (!currentData) {return [];}
+
+    return currentData.map((d) => {
+        const index = newData.findIndex((newD) => newD._id === d._id);
+        if (index === -1) {return d;}
+        return newData[index];
+    });
+};
+
+const onRemoved = (currentData: any[], removedData: any[]) => {
+    if (!currentData) {return [];}
+    return currentData
+        .filter(
+            (d) => removedData.findIndex((newD) => newD._id === d._id)
+        );
+};
+
+const updateResult = (state: ResultState, event: StoreChangeEvent) => {
+    switch (event.eventType) {
+        case CRUDEvents.ADD:
+            return onAdded(state.data, event.data);
+
+        case CRUDEvents.UPDATE:
+            return onChanged(state.data, event.data);
+
+        case CRUDEvents.DELETE:
+            return onRemoved(state.data, event.data);
+
+        default:
+            throw new Error(`Invalid event ${event.eventType} received`);
+    }
+};
+
+const createSubscribeToMore = (state: ResultState, model: Model, dispatch: Dispatch<Action>) => {
+    return (eventsToWatch: CRUDEvents[], customEventHandler?: (state: ResultState, data: any) => any) => {
+        // TODO subscribe to specific document
         const subscription = model.subscribe((event) => {
-            const newData = updateResult(event.data);
-            if (subscription.closed) {
-                // Important to check beacuse Componnent could be unmounted
-                return;
+            let newData;
+
+            if (customEventHandler) {
+                newData = customEventHandler(state, event.data);
             }
-            dispatch({ type: ActionType.UPDATE_RESULT, data: newData });
-        }, eventType);
+            newData = updateResult(state, event);
+
+            if (!subscription.closed) {
+                // Important to check beacuse Componnent could be unmounted
+                dispatch({ type: ActionType.UPDATE_RESULT, data: newData });
+            }
+        }, eventsToWatch);
         return subscription;
     };
 };
 
 export const useQuery = (model: Model, selector?: Filter | string) => {
     const [state, dispatch] = useReducer(reducer, InitialState);
-    const subscribeToMore = useCallback(createSubscribeToMore(model, dispatch), [model, dispatch]);
+    const subscribeToMore = useCallback(
+        createSubscribeToMore(state, model, dispatch), [state, model, dispatch]
+    );
 
     useEffect(() => {
         (async () => {
@@ -46,6 +92,9 @@ export const useQuery = (model: Model, selector?: Filter | string) => {
 
 export const useLazyQuery = (model: Model) => {
     const [state, dispatch] = useReducer(reducer, InitialState);
+    const subscribeToMore = useCallback(
+        createSubscribeToMore(state, model, dispatch), [state, model, dispatch]
+    );
 
     const query = async (selector?: Filter | string) => {
         if (state.isLoading) { return; }
@@ -62,14 +111,7 @@ export const useLazyQuery = (model: Model) => {
         } catch (error) {
             dispatch({ type: ActionType.REQUEST_COMPLETE, error });
         }
-
-        /*
-         * Each time the query func is called, a new subscribeToMore
-         * funtion is created. The user has to manually unsuscribe from previous
-         * subscriptions
-         */
-        return createSubscribeToMore(model, dispatch);
     };
 
-    return { ...state, query };
+    return { ...state, query, subscribeToMore };
 };
