73adf9265aa9a847489631ac38761549a5e14acf
Gianluca <gianlucazuccarelli@gmail.com>
2020-08-21 11:49:02 +0100

    fix: remove adapter exports

--- packages/datastore/datastore/src/storage/index.ts
+++ packages/datastore/datastore/src/storage/index.ts
@@ -2,6 +2,4 @@ export * from "./LocalStorage";
 export * from "./api/CRUDEvents";
 export * from "./api/StorageAdapter";
 export * from "./api/StoreChangeEvent";
-export { WebSQLAdapter } from "./adapters/sqlite/WebSQLAdapter";
-export { SQLiteAdapter } from "./adapters/sqlite/SQLiteAdapter";
 
