889625032568704452890a3338a132fdb90b6072
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-08-21 17:02:47 +0100

    Update docs

--- packages/datastore/datastore/src/react/hooks/subscription.ts
+++ packages/datastore/datastore/src/react/hooks/subscription.ts
@@ -8,7 +8,7 @@ export const useSubscription = (model: Model, eventTypes: CRUDEvents[], filter?:
   const [state, dispatch] = useReducer(reducer, InitialState);
 
   useEffect(() => {
-    // TODO subcribe using predicate
+    // TODO subcribe using filter
     const subscription = model.subscribe((event) => {
       dispatch({ type: ActionType.REQUEST_COMPLETE, data: event.data });
     }, eventTypes);
