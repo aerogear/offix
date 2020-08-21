d6e33a4d623a77cc11f14cac93cfc0acbae27477
Wojciech Trocki <wtrocki@redhat.com>
2020-08-21 15:04:10 +0100

    fix: split cmd and package main

--- packages/datastore/cli/src/index.ts
+++ packages/datastore/cli/src/index.ts
@@ -4,19 +4,19 @@ import { generate } from "./generate";
 export { generate };
 export { OffixDataSyncPlugin as Plugin } from "./OffixDataSyncPlugin";
 
-if (require.main === module) {
-    // eslint-disable-next-line no-unused-expressions
-    yargs
-        .command(
-            "generate",
-            "Generate Datastore config",
-            {
-                schema: { describe: "The schema path" },
-                outputPath: { describe: "The output dir path" }
-            },
-            (argv) => generate(argv.schema as string, argv.outputPath as string)
-        )
-        .demandCommand(1)
-        .help()
-        .argv;
+export const cmd = () => {
+  // eslint-disable-next-line no-unused-expressions
+  yargs
+    .command(
+      "generate",
+      "Generate Datastore config",
+      {
+        schema: { describe: "The schema path" },
+        outputPath: { describe: "The output dir path" }
+      },
+      (argv) => generate(argv.schema as string, argv.outputPath as string)
+    )
+    .demandCommand(1)
+    .help()
+    .argv;
 }
