57ca47c15f3239ad0941051c0a544442a758c60a
Gianluca <gianlucazuccarelli@gmail.com>
2020-08-24 10:45:28 +0100

    chore: implement sqlite saveOrUpdate

--- packages/datastore/datastore/src/storage/adapters/sqlite/SQLiteAdapter.ts
+++ packages/datastore/datastore/src/storage/adapters/sqlite/SQLiteAdapter.ts
@@ -12,7 +12,17 @@ export class SQLiteAdapter extends AbstractSQLAdapter implements StorageAdapter 
       dbName,
       schemaVersion.toString(),
       "Offix datastore",
+      // @ts-ignore
       5 * 1024 * 1024
     ));
   }
+
+  public async saveOrUpdate(storeName: string, idField: string, input: any) {
+    const store = await this.queryById(storeName, idField, input[idField]);
+    if (!store.length) {
+      return this.save(storeName, input);
+    }
+    return this.update(storeName, input, { [idField]: input[idField] });
+  }
+
 }
