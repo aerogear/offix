8d37fb1703e18e08518e38bef3156180f36b89d9
Wojciech Trocki <wtrocki@redhat.com>
2020-09-23 18:04:49 +0100

    fix: add ability to use target schema for model

--- packages/datastore/cli/src/generate-documents/createModelTypes.ts
+++ packages/datastore/cli/src/generate-documents/createModelTypes.ts
@@ -3,30 +3,35 @@ import { GraphQLOutputType, isNonNullType, getNullableType } from "graphql";
 import { convertToTsType } from "../utils";
 
 const getField = (fieldName: string, type: GraphQLOutputType) => {
-    if (isNonNullType(type)) {
-        type = getNullableType(type);
-    } else {
-        fieldName = `${fieldName}?`;
-    }
+  if (isNonNullType(type)) {
+    type = getNullableType(type);
+  } else {
+    fieldName = `${fieldName}?`;
+  }
 
-    return `${fieldName}: ${convertToTsType(type)}`;
+  return `${fieldName}: ${convertToTsType(type)}`;
 };
 
 const getModelProperties = (model: ModelDefinition) => {
-    const fieldMap = model.graphqlType.getFields();
+  const fieldMap = model.graphqlType.getFields();
 
-    return Object.keys(fieldMap)
-        .map(fieldName => (getField(fieldName, fieldMap[fieldName].type)))
-        .join(";\n    ");
+  return Object.keys(fieldMap)
+    .map(fieldName => {
+      if (fieldName === "_version") {
+        // We want to add our own version
+        return "";
+      }
+      return getField(fieldName, fieldMap[fieldName].type);
+    })
+    .join(";\n    ") + "   _version: number;\n    ";
 };
 
 export const createModelType = (model: ModelDefinition) => {
-    const modelName = model.graphqlType.name;
-    const primaryKey = getPrimaryKey(model.graphqlType).name;
+  const modelName = model.graphqlType.name;
+  const primaryKey = getPrimaryKey(model.graphqlType).name;
 
-    return `export interface ${modelName} {
+  return `export interface ${modelName} {
     ${getModelProperties(model)}
-    _version: number;
 }
 
 export type ${modelName}Create = ${primaryKey ? `Omit<${modelName}, "${primaryKey}">` : modelName};
