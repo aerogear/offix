a3b8ef48342fb6416c2301c6ea68e8d855dc4d47
Gianluca <gzuccare@redhat.com>
2020-10-15 15:13:33 +0100

    chore: refactor createModelType to use jsonSchema

--- packages/datastore/cli/src/generate-documents/createModelTypes.ts
+++ packages/datastore/cli/src/generate-documents/createModelTypes.ts
@@ -1,4 +1,4 @@
-import { ModelDefinition, getPrimaryKey } from "@graphback/core";
+// import { ModelDefinition, getPrimaryKey } from "@graphback/core";
 import { GraphQLOutputType, isNonNullType, getNullableType } from "graphql";
 import { convertToTsType } from "../utils";
 
@@ -9,27 +9,45 @@ const getField = (fieldName: string, type: GraphQLOutputType) => {
         fieldName = `${fieldName}?`;
     }
 
-    return `${fieldName}: ${convertToTsType(type)}`;
+    // return `${fieldName}: ${convertToTsType(type)}`;
+    return `${fieldName}: ${type}`;
 };
 
-const getModelProperties = (model: ModelDefinition) => {
-    const fieldMap = model.graphqlType.getFields();
+const getModelProperties = (schema: any) => {
+    const fieldMap = schema.properties;
+    const keys = Object.keys(fieldMap);
+    // const relNames = model.relationships.map(r => r.ownerField.name);
 
-    return Object.keys(fieldMap)
-        .map(fieldName => (getField(fieldName, fieldMap[fieldName].type)))
-        .join(";\n    ");
+    // // Create a set to remove duplicates
+    // const relationships = new Set(model.relationships
+    //   // filter out oneToMany fields
+    //   .filter(r => r.kind !== "oneToMany")
+    //   // get fields for relationships
+    //   .map(r => (getField(r.relationForeignKey!, r.relationType)))
+    //  );
+
+    return keys
+    //   // filter out auto generated relationship fields
+    //   .filter(fieldName => !relNames.includes(fieldName))
+      .map(fieldName => {
+        const s = fieldMap[fieldName];
+        const name = s.isRequired ? fieldName : `${fieldName}?`;
+        return `${name}: ${s.type}`;
+      })
+    //   // add fields from relationships (with foreign key)
+    //   .concat(Array.from(relationships))
+      .join(";\n    ");
 };
 
-export const createModelType = (model: ModelDefinition) => {
-    const modelName = model.graphqlType.name;
-    const primaryKey = getPrimaryKey(model.graphqlType).name;
+export const createModelType = (schema: any) => {
+    const { name: modelName, primaryKey } = schema;
+ 
+    return `
+      export interface ${modelName} {
+          ${getModelProperties(schema)}
+      }
 
-    return `export interface ${modelName} {
-    ${getModelProperties(model)}
-    _version: number;
-}
-
-export type ${modelName}Create = ${primaryKey ? `Omit<${modelName}, "${primaryKey}">` : modelName};
-export type ${modelName}Change =  ${primaryKey ? `Pick<${modelName}, "${primaryKey}"> & ` : ""}Partial<${modelName}Create>;
-`;
+      export type ${modelName}Create = ${primaryKey ? `Omit<${modelName}, "${primaryKey}">` : modelName};
+      export type ${modelName}Change =  ${primaryKey ? `Pick<${modelName}, "${primaryKey}"> & ` : ""}Partial<${modelName}Create>;
+  `;
 };
